268ebefd54740c549f322b9ac37fda871d3c8beb
Potter, John A <john.a.potter@jpmchase.com>
2022-07-22 19:06:14 +0100

    docs: add gatsby example (UIE-6963)

---
+++ packages/example-gatsby-static/gatsby-config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter MDX Basic',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@chrisbiscardi',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: { default: path.resolve('./src/components/layout.js') },
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ]
+}
