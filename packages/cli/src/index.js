498de905df0846713896b14badccb7b0de1af266
lilyvc <45168453+lilyvc@users.noreply.github.com>
2022-11-03 11:13:24 +0000

    Generate and serve snapshots (#33)
    * add functionality to generate and serve snapshots
    
    * update readme with snapshot commands
    
    * add --snapshot option to cli

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -2,31 +2,47 @@
 
 const { program } = require('commander');
 const serve = require('./serve');
+const serveSnapshot = require('./serve-snapshot');
 const build = require('./build');
 const path = require('path');
 
 program
-  .requiredOption('-c, --config <string>', 'Config path')
+  .option('-c, --config <string>', 'Config path')
   .option('-o, --out <string>', 'Output directory', '.tmp/.mosaic-build')
   .option('-s, --scope <strings>', 'Command separated namespaces')
-  .option('-p, --port <number>', 'Port to serve on', 8080);
+  .option('-p, --port <number>', 'Port to serve on', 8080)
+  .option('-S, --snapshot <string>', 'Snapshot path');
 
 program.parse();
 
 const options = program.opts();
 
-const config = require(path.resolve(process.cwd(), options.config));
-
-if (!config) {
-  throw new Error(`Could not find config file at ${path.resolve(process.cwd(), options.config)}.`);
-}
-
 if (program.args[0] === 'build') {
-  build(
-    config,
-    path.resolve(process.cwd(), options.out),
-    options.scope && options.scope.split(',')
-  );
+  if (options.config !== undefined) {
+    const config = require(path.resolve(process.cwd(), options.config));
+    if (!config) {
+      throw new Error(
+        `Could not find config file at ${path.resolve(process.cwd(), options.config)}.`
+      );
+    }
+    build(
+      config,
+      path.resolve(process.cwd(), options.out),
+      options.scope && options.scope.split(',')
+    );
+  }
 } else if (program.args[0] === 'serve') {
-  serve(config, options.port, options.scope && options.scope.split(','));
+  if (options.snapshot !== undefined) {
+    const targetDir = path.resolve(process.cwd(), options.snapshot);
+    serveSnapshot(targetDir, options.port, options.scope && options.scope.split(','));
+  }
+  if (options.config !== undefined) {
+    const config = require(path.resolve(process.cwd(), options.config));
+    if (!config) {
+      throw new Error(
+        `Could not find config file at ${path.resolve(process.cwd(), options.config)}.`
+      );
+    }
+    serve(config, options.port, options.scope && options.scope.split(','));
+  }
 }
