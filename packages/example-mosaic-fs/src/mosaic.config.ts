5f1f03cb4cfebe4f5ce5e1173ad72662357973f9
Tam Saunders <thomas.j.saunders@jpmchase.com>
2022-11-15 18:08:07 +0000

    remove bitbucket source from example fs

--- packages/example-mosaic-fs/src/mosaic.config.ts
+++ packages/example-mosaic-fs/src/mosaic.config.ts
@@ -83,31 +83,10 @@ const config: MosaicConfig = {
       modulePath: require.resolve('@jpmorganchase/mosaic-source-local-folder'),
       namespace: 'local',
       options: {
-        rootDir: path.join('../developer-docs', 'docs'),
+        rootDir: path.join(__dirname, '../', 'docs'),
         cache: true,
-        prefixDir: 'local',
+        prefixDir: 'example',
         extensions: ['.mdx']
-      }
-    },
-    {
-      modulePath: require.resolve('@jpmorganchase/mosaic-source-bitbucket'),
-      namespace: 'developer', // each site has it's own namespace, think of this as your content's uid
-      workflows: [BitBucketPullRequestWorkflow],
-      options: {
-        // To run locally, enter your credentials to access the BitBucket repo
-        // !! Polite Reminder... do not store credentials in code !!
-        // For final deployments, you could put repo access credentials securely in environment variables provided by Gaia console.
-        // credentials: "{process.env.FID}:{process.env.FID_PERSONAL_ACCESS_TOKEN}",
-        // If running locally
-        // create an environment variable like BITBUCKET_CLONE_CREDENTIALS to let the user define it via the CLI
-        // export BITBUCKET_CLONE_CREDENTIALS="<sid>:<Personal Access Token (PAT) provided by your Repo OR password>",
-        prefixDir: 'developer',
-        credentials: process.env.BITBUCKET_CLONE_CREDENTIALS,
-        subfolder: 'docs', // subfolder within your branch containing the docs, typically 'docs'
-        repo: process.env.DEVELOPER_DOCS_REPO, // repo url without any protocol
-        branch: 'develop', // branch where docs are pulled from
-        extensions: ['.mdx'], // extensions of content which should be pulled
-        remote: 'origin' // what is the shorthand name of the remote repo, typically 'origin'
       }
     }
   ]
