637f8cfda0f779408323d1ff534470251e04ba08
Davie <reid5587@gmail.com>
2022-11-15 17:14:02 +0000

    use zod schemas for mosaic config

---
+++ packages/schemas/src/index.ts
@@ -0,0 +1,6 @@
+export * from './PluginModuleSchema';
+export * from './MosaicConfigSchema';
+export * from './SerialiserModuleSchema';
+export * from './SourceModuleSchema';
+export * from './SourceWorkflowSchema';
+export * from './fileExtensionSchema';
--- packages/types/src/SerialiserModuleDefinition.ts
+++
@@ -1,5 +0,0 @@
-import type { PluginModuleDefinition } from './PluginModuleDefinition';
-
-export type SerialiserModuleDefinition = PluginModuleDefinition & {
-  filter: RegExp;
-};
