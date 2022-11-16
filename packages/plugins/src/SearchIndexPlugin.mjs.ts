bfc27e020aee33d17ae7371dc0ed5f0c16147bfe
Tom Hazledine <tom.hazledine@jpmorgan.com>
2022-11-16 14:31:57 +0000

    feat: add page titles to search-data json

--- packages/plugins/src/SearchIndexPlugin.mjs.ts
+++ packages/plugins/src/SearchIndexPlugin.mjs.ts
@@ -1,8 +1,29 @@
 import type { Page, Plugin as PluginType } from '@jpmorganchase/mosaic-types';
 
 const SearchIndexPlugin: PluginType<Page> = {
-  async afterUpdate(_, { sharedFilesystem }) {
-    const searchData = {};
+  async afterUpdate(mutableFilesystem, { sharedFilesystem, serialiser, ignorePages }) {
+    const pages = await Promise.all(
+      (
+        (await mutableFilesystem.promises.glob('**', {
+          onlyFiles: true,
+          ignore: ignorePages.map(ignore => `**/${ignore}`),
+          cwd: '/'
+        })) as string[]
+      ).map(async pagePath => {
+        const deserialisedPage = await serialiser.deserialise(
+          pagePath,
+          await mutableFilesystem.promises.readFile(pagePath)
+        );
+        return deserialisedPage;
+      })
+    );
+
+    const searchData = pages.map(page => {
+      return {
+        title: page.title
+      };
+    });
+
     await sharedFilesystem.promises.writeFile('/search-data.json', JSON.stringify(searchData));
   }
 };
