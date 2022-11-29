8be1f14f7f4fe5cca5ca553303fff1e39c29e4d1
Luke Atkinson-Coyle <luke.atkinson-coyle@jpmorgan.com>
2022-11-29 18:03:55 +0000

    init alexjs

---
+++ .alexrc.js
@@ -0,0 +1,10 @@
+//TODO: pre-commit hook for Alex, see how is done for prettier
+
+exports.noBinary = true;
+exports.profanitySureness = 1;
+exports.allow = [
+   "example1",
+    "example2"
+];
+
+
