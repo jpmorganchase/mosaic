1f22009ac14bc5a9941aff85012f2c441c5c83a7
lukeac123 <96307810+lukeac123@users.noreply.github.com>
2022-12-01 11:42:41 +0000

    Intial open api component (#83)
    inital open-api-component commit

---
+++ packages/open-api-component/src/OpenAPI/index.tsx
@@ -0,0 +1,22 @@
+import React, { lazy } from 'react';
+
+import { IsomorphicSuspense } from '../IsomorphicSuspense';
+
+export interface OpenAPIProps {
+  /** Additional class name for root class override */
+  className?: string;
+  /* path to Swagger API instance */
+  url?: string;
+}
+
+const LazySwagger = lazy(() =>
+  import('./SwaggerUi.js').then(x => {
+    return x.default;
+  })
+);
+
+export const OpenAPI: React.FC<OpenAPIProps> = ({ url }) => (
+  <IsomorphicSuspense fallback={<span>Loading Swagger component...</span>}>
+    <LazySwagger url={url} />
+  </IsomorphicSuspense>
+);
---
+++ packages/open-api-component/src/OpenAPI/styles.css.ts
@@ -0,0 +1,6 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('.swagger-ui pre code', {
+  color: 'white !important',
+  textShadow: 'none !important'
+});
