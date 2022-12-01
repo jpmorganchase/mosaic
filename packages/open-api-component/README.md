909b2eb3f25cdab3c98a59489777f92c76782f36
lukeac123 <96307810+lukeac123@users.noreply.github.com>
2022-12-01 11:42:41 +0000

    Intial open api component (#83)
    inital open-api-component commit

---
+++ packages/open-api-component/README.md
@@ -0,0 +1,7 @@
+# Mosaic Open API Component
+
+`@jpmorganchase/components-open-api` is an Open API UI component for documenting APIs.
+
+## Installation
+
+`yarn add @jpmorganchase/components-open-api`
---
+++ packages/open-api-component/src/OpenAPI/SwaggerUi.tsx
@@ -0,0 +1,9 @@
+/**
+ * Keep this separate.
+ * We want this to be loaded on the client and not the server to prevent
+ * issues with swagger-ui-react dependencies.
+ */
+
+import SwaggerUI from 'swagger-ui-react';
+
+export default SwaggerUI;
