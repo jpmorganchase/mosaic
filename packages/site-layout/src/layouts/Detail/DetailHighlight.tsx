404be75d73b26b7b9939ff15fd8c9307f51e840c
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2022-12-01 13:57:24 +0000

    initial-site-layout commit

---
+++ packages/site-layout/src/layouts/Detail/DetailHighlight.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AppHeader, Breadcrumbs, Footer, TableOfContents } from '@jpmorganchase/mosaic-site-components';
+
+import { LayoutBase } from '../../LayoutBase';
+import { LayoutColumns } from '../../LayoutColumns';
+import type { LayoutProps } from '../../types';
+
+export const DetailHighlight: React.FC<LayoutProps> = ({ FooterProps, children }) => (
+  <LayoutBase Header={<AppHeader />}>
+    <LayoutColumns
+      PrimarySidebar={null}
+      SecondarySidebar={<TableOfContents />}
+      Footer={<Footer {...FooterProps} />}
+    >
+      <Breadcrumbs />
+      {children}
+    </LayoutColumns>
+  </LayoutBase>
+);
