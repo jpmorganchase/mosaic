7e5d997034e1df336caea9be2f66eece040aa1c0
Tam Saunders <tambo@users.noreply.github.com>
2022-12-02 16:54:47 +0000

    initial create-site package (#85)
    * initial create-site package
    
    * license change
    
    * Update init.ts
    
    remove unwanted reference
    
    * Update package.json
    
    Co-authored-by: Tam Saunders <tambo@users.noreply.github.com>
    Co-authored-by: Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
    Co-authored-by: mark-tate <143323+mark-tate@users.noreply.github.com>
    Co-authored-by: lilyvc <45168453+lilyvc@users.noreply.github.com>

---
+++ packages/create-site/src/index.ts
@@ -0,0 +1,16 @@
+const standardGeneratorConfig = require('@jpmorganchase/standard-generator/dist/generator.config.js');
+
+/**
+ * Get list of Mosaic default generators, can be composed with your own generators
+ */
+const getGenerators = () => [
+  ['@jpmorganchase/standard-generator/dist/generator', standardGeneratorConfig]
+];
+
+module.exports = {
+  /**
+   * This is an extension point for modifying existing generators/configs or adding new ones.
+   * They will appear as options within the users create-mosaic-site interactive menu
+   */
+  getGenerators
+};
