87ea10c7601cdd9a7405de340e7557033d97f0ed
mark-tate <143323+mark-tate@users.noreply.github.com>
2022-12-02 16:36:27 +0000

    Initial mosaic generator (#97)
    * inital standard-generator commit
    
    * prettier was ignoring files from gitignore rather than prettierignore
    
    Co-authored-by: Luke Atkinson-Coyle <luke.atkinson-coyle@jpmorgan.com>

---
+++ packages/standard-generator/src/templates/src/types/mosaic.ts
@@ -0,0 +1,28 @@
+import type {
+  MosaicAppProps,
+  MiddlewarePresetsProps
+} from '@jpmorganchase/mosaic-site-components/dist/utils/middlewarePresets';
+
+/**
+ * In this file, define each of your own Middleware props, which combine together into MyAppProps
+ *
+ * Define the props returned by your middleware
+ *
+ * export interface ExampleMiddlewareProps {
+ *   someProp?: string;
+ * }
+ *
+ * Insert the options you want to pass to your middleware (optional)
+ *
+ * export interface ExampleMiddlewareOptions {
+ *   someOption: number;
+ * }
+ *
+ * Then compose all your own Middleware props with the default MosaicAppProps
+ *
+ * export interface MyAppProps extends MosaicAppProps<MyMiddlewareProps> {};
+ */
+
+export declare type MyMiddlewareProps = MiddlewarePresetsProps /* & ExampleMiddlewareOptions */;
+
+export interface MyAppProps extends MosaicAppProps<MyMiddlewareProps> {}
