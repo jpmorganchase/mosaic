a2dc13bf90687e15a79ff2b2cd5de2d0029efdca
Davie <reid5587@gmail.com>
2022-12-08 12:22:46 +0000

    use @swc/jest for tests

--- jest.config.server.js
+++ jest.config.server.js
@@ -1,9 +1,15 @@
 const sharedConfig = require('./scripts/jest/jest.config.base.js');
 
+const moduleNameMapper = {
+  '^(\\.{1,2}/.*)\\.js$': '$1',
+  '^lodash-es$': 'lodash'
+};
+
 module.exports = {
   displayName: 'Server Tests',
   ...sharedConfig,
   testEnvironment: 'node',
+  moduleNameMapper,
   setupFiles: ['./scripts/jest/server/jest.setup.js'],
   setupFilesAfterEnv: ['./scripts/jest/server/jest.environment.js'],
   // Add tests paths to roots
