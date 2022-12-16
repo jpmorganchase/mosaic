752ad0f85928cd63ea6510388c049a054e7c8434
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2022-12-16 16:46:34 +0000

    mosaic provider

--- packages/site-components/src/MosaicProvider.tsx
+++ packages/site-components/src/MosaicProvider.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import Head from 'next/head';
-import { BaseUrlProvider } from './BaseUrlProvider';
-import { Image } from './Image';
 import { Link } from './Link';
-import { Metadata } from './Metadata';
 import { SidebarProvider } from './SidebarProvider';
-import { ImageProvider, LinkProvider, ThemeProvider } from '@jpmorganchase/mosaic-components';
+import { LinkProvider, ThemeProvider } from '@jpmorganchase/mosaic-components';
 import { useCreateStore, StoreProvider } from '@jpmorganchase/mosaic-store';
 
 export const MosaicProvider = ({ storeProps, children }) => {
   const createStore = useCreateStore(storeProps);
   return (
     <StoreProvider value={createStore()}>
-      <Metadata Component={Head} />
       <ThemeProvider>
-        <BaseUrlProvider>
-          <ImageProvider value={Image}>
-            <LinkProvider value={Link}>
-              <SidebarProvider>{children}</SidebarProvider>
-            </LinkProvider>
-          </ImageProvider>
-        </BaseUrlProvider>
+        <LinkProvider value={Link}>
+          <SidebarProvider>{children}</SidebarProvider>
+        </LinkProvider>
       </ThemeProvider>
     </StoreProvider>
   );
