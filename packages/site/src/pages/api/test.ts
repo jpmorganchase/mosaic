d4ade5d341683c18fb70a01f9f6cee4c358c7ea4
Davie <reid5587@gmail.com>
2022-12-20 11:03:23 +0000

    try respond with error in test api

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -3,10 +3,12 @@ import path from 'path';
 
 export default function handler(req, res) {
   const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR as string;
-  // Find the absolute path for  the file/dir requested
   const file = path.join(process.cwd(), mosaicSnapshotDir, 'mosaic', 'shared-config.json');
-  const stringified = readFileSync(file, 'utf8');
-
   res.setHeader('Content-Type', 'application/json');
-  return res.end(stringified);
+  try {
+    const stringified = readFileSync(file, 'utf8');
+    return res.end(stringified);
+  } catch (e) {
+    return res.end({ file, e });
+  }
 }
