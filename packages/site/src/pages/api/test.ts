5f06b2bc79b289f4e8daa51bba3f8b11e270a627
Davie <reid5587@gmail.com>
2022-12-20 16:08:05 +0000

    read shared-config

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
+import { promises as fs } from 'fs';
 
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies
-  });
+export default async function handler(_request: NextApiRequest, response: NextApiResponse) {
+  const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR || '';
+  const snapshotDir = path.join(process.cwd(), mosaicSnapshotDir, 'mosaic');
+  path.resolve(snapshotDir);
+  const fileContents = await fs.readFile(path.join(snapshotDir, 'shared-config.json'), 'utf8');
+  response.status(200).json(fileContents);
 }
