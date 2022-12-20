f0272e8269148a219e41dedba1613eb537fb4f2d
Davie <reid5587@gmail.com>
2022-12-20 11:52:14 +0000

    hardcode snapshot dir

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -1,14 +1,9 @@
-import { readFileSync } from 'fs';
-import path from 'path';
-
 export default function handler(req, res) {
-  const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR as string;
-  const file = path.join(process.cwd(), mosaicSnapshotDir, 'mosaic', 'shared-config.json');
   res.setHeader('Content-Type', 'application/json');
   try {
-    const stringified = readFileSync(file, 'utf8');
-    return res.end(stringified);
+    const dir = process.env.MOSAIC_SNAPSHOT_DIR || 'env variable undefined';
+    return res.end(dir);
   } catch (e) {
-    return res.end({ file, e });
+    return res.end('error');
   }
 }
