2884c52bb9affac7160fa07f50b14e4567815a9e
Davie <reid5587@gmail.com>
2022-12-20 14:41:25 +0000

    test api should get shared-config

--- packages/site/src/pages/_app.tsx
+++ packages/site/src/pages/_app.tsx
@@ -21,8 +21,6 @@ import { MyAppProps } from '../types/mosaic';
 const components = mosaicComponents;
 const layoutComponents = mosaicLayouts;
 
-console.log('MOSAIC URL', process.env.MOSAIC_URL);
-
 export default function MyApp({ Component, pageProps = {} }: AppProps<MyAppProps>) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
