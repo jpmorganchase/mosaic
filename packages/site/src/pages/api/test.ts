917637b63e8fe22a4f642dc9736fb44a0042daa0
Mark Tate <143323+mark-tate@users.noreply.github.com>
2022-12-21 21:00:59 +0100

    remove public

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -5,13 +5,7 @@ import { promises as fs } from 'fs';
 export default async function handler(_request: NextApiRequest, response: NextApiResponse) {
   const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR || '';
   const rootDir = __dirname.split('.next')[0];
-  const sharedConfigPath = path.join(
-    rootDir,
-    // 'public',
-    mosaicSnapshotDir,
-    'mosaic',
-    'shared-config.json'
-  );
+  const sharedConfigPath = path.join(rootDir, mosaicSnapshotDir, 'mosaic', 'shared-config.json');
   console.log('>> sharedConfigPath', sharedConfigPath);
   const fileContents = await fs.readFile(sharedConfigPath, 'utf8');
   response.status(200).json(fileContents);
