978ee8b528e3aba25a79e31adcf306e215525584
Davie <reid5587@gmail.com>
2022-12-21 16:23:11 +0000

    move snapshots from public dir

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -4,8 +4,8 @@ import { promises as fs } from 'fs';
 
 export default async function handler(_request: NextApiRequest, response: NextApiResponse) {
   const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR || '';
-  const snapshotDir = path.resolve(mosaicSnapshotDir, 'mosaic');
-  path.resolve(snapshotDir);
+  const snapshotDir = path.join(process.cwd(), mosaicSnapshotDir, 'mosaic');
   const fileContents = await fs.readFile(path.join(snapshotDir, 'shared-config.json'), 'utf8');
+  response.setHeader('Content-Type', 'application/json');
   response.status(200).json(fileContents);
 }
