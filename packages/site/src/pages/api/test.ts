ed21ce48b8304a34371439c0e41979ef6db334c8
Davie <reid5587@gmail.com>
2022-12-22 09:52:35 +0000

    return mosaic url env

--- packages/site/src/pages/api/test.ts
+++ packages/site/src/pages/api/test.ts
@@ -1,11 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
-import { promises as fs } from 'fs';
 
 export default async function handler(_request: NextApiRequest, response: NextApiResponse) {
-  const mosaicSnapshotDir = process.env.MOSAIC_SNAPSHOT_DIR || '';
-  const snapshotDir = path.join(process.cwd(), mosaicSnapshotDir, 'mosaic');
-  const fileContents = await fs.readFile(path.join(snapshotDir, 'shared-config.json'), 'utf8');
   response.setHeader('Content-Type', 'application/json');
-  response.status(200).json(fileContents);
+  response.status(200).json({ api: process.env.MOSAIC_URL });
 }
