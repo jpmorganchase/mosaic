f17c2624bf4aa5903bfec547fc993cc84b1c0743
Davie <reid5587@gmail.com>
2022-12-28 14:08:05 +0000

    add site package

---
+++ packages/site/src/pages/[...route].tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { Body } from '@jpmorganchase/mosaic-site-components';
+import {
+  createMiddlewareRunner,
+  MiddlewareResult
+} from '@jpmorganchase/mosaic-site-components/dist/utils/createMiddlewareRunner';
+import { middlewarePresets } from '@jpmorganchase/mosaic-site-components/dist/utils/middlewarePresets';
+
+import type { MyAppProps, MyMiddlewareProps } from '../types/mosaic';
+
+/**
+ * Extend props passed to MyApp by adding your own middleware ('withMyExampleMiddleware') functions.
+ *
+ const middlewareRunner = createMiddlewareRunner<ExampleMiddlewareProps, ExampleMiddlewareOptions>({}, 
+   [
+     ...middlewarePresets,
+     [withMyExampleMiddleware, { someProp: 20000 }]
+   ]
+ );
+ */
+const middlewareRunner = createMiddlewareRunner<MyMiddlewareProps>({}, middlewarePresets);
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<Partial<GetServerSidePropsResult<MiddlewareResult<MyAppProps>>>> {
+  const props = await middlewareRunner(context, {});
+  return props;
+}
+
+/** MyApp will be passed MyAppProps which is created by combining the result of all props created by Middleware */
+const MyApp = props => <Body {...props} />;
+export default MyApp;
