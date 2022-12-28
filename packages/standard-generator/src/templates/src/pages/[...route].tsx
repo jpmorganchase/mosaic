806541d4e29beefae9cfef5d69fd6fc99baa4a72
Davie <reid5587@gmail.com>
2022-12-28 14:30:17 +0000

    await middleware runner result in generator template

--- packages/standard-generator/src/templates/src/pages/[...route].tsx
+++ packages/standard-generator/src/templates/src/pages/[...route].tsx
@@ -24,7 +24,8 @@ const middlewareRunner = createMiddlewareRunner<MyMiddlewareProps>({}, middlewar
 export async function getServerSideProps(
   context: GetServerSidePropsContext
 ): Promise<Partial<GetServerSidePropsResult<MiddlewareResult<MyAppProps>>>> {
-  return middlewareRunner(context, {});
+  const props = middlewareRunner(context, {});
+  return props;
 }
 
 /** MyApp will be passed MyAppProps which is created by combining the result of all props created by Middleware */
