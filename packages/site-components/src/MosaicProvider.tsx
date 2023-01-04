9d636800f2650e0ed801448bd2c020735de3b151
Luke Atkinson-Coyle <luke.atkinson-coyle@jpmorgan.com>
2023-01-04 14:10:46 +0000

    mere store, theme and layout into single mosaic provider

--- packages/site-components/src/MosaicProvider.tsx
+++ packages/site-components/src/MosaicProvider.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from './Link';
 import { SidebarProvider } from './SidebarProvider';
-import { LinkProvider, ThemeProvider } from '@jpmorganchase/mosaic-components';
+import { ThemeProvider } from '@jpmorganchase/mosaic-components';
 import { useCreateStore, StoreProvider } from '@jpmorganchase/mosaic-store';
+import { LayoutProvider } from '@jpmorganchase/mosaic-layouts';
+import { layouts as mosaicLayouts } from '@jpmorganchase/mosaic-layouts';
 
 export const MosaicProvider = ({ storeProps, children }) => {
   const createStore = useCreateStore(storeProps);
   return (
     <StoreProvider value={createStore()}>
       <ThemeProvider>
-        <LinkProvider value={Link}>
-          <SidebarProvider>{children}</SidebarProvider>
-        </LinkProvider>
+        <SidebarProvider>
+          <LayoutProvider layoutComponents={mosaicLayouts}>{children}</LayoutProvider>
+        </SidebarProvider>
       </ThemeProvider>
     </StoreProvider>
   );
