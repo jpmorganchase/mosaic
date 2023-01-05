e70230216b9464d962170a56111f93f300ea121e
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2023-01-05 11:18:44 +0000

    merge store, layout, theme and sidebar providers

---
+++ packages/site-components/src/MosaicProvider.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SidebarProvider } from './SidebarProvider';
+import { ThemeProvider } from '@jpmorganchase/mosaic-components';
+import { useCreateStore, StoreProvider } from '@jpmorganchase/mosaic-store';
+import { LayoutProvider } from '@jpmorganchase/mosaic-layouts';
+import { layouts as mosaicLayouts } from '@jpmorganchase/mosaic-layouts';
+
+export const MosaicProvider = ({ storeProps, children }) => {
+  const createStore = useCreateStore(storeProps);
+
+  return (
+    <StoreProvider value={createStore()}>
+      <ThemeProvider>
+        <SidebarProvider>
+          <LayoutProvider layoutComponents={mosaicLayouts}>{children}</LayoutProvider>
+        </SidebarProvider>
+      </ThemeProvider>
+    </StoreProvider>
+  );
+};
