f9cbf8cb26eece587976ea3aa1cda4c560558a07
Davie <reid5587@gmail.com>
2023-01-09 13:19:04 +0000

    update testing libraries

--- packages/store/src/__tests__/useBreadcrumbs.test.ts
+++ packages/store/src/__tests__/useBreadcrumbs.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 
 import { createWrapper } from './test-utils/utils';
 import { type Breadcrumb, useBreadcrumbs } from '../useBreadcrumbs';
--- packages/store/src/__tests__/useMeta.test.ts
+++ packages/store/src/__tests__/useMeta.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 
 import { createWrapper } from './test-utils/utils';
 import { useMeta } from '../useMeta';
