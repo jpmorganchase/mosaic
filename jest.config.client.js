4e953641ab7cd44021e126a7758eeeb4d37d22de
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-01-15 00:00:07 +0000

    fix tests failure due to adding exports to package.json

--- jest.config.client.js
+++ jest.config.client.js
@@ -3,7 +3,10 @@ const sharedConfig = require('./scripts/jest/jest.config.base.js');
 const moduleNameMapper = {
   '\\.(css)$': 'identity-obj-proxy',
   '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    '<rootDir>/__mocks__/file-stub.js'
+    '<rootDir>/__mocks__/file-stub.js',
+  '@jpmorganchase/mosaic-components': '<rootDir>/packages/components/dist/index.js',
+  '@jpmorganchase/mosaic-store': '<rootDir>/packages/store/dist/index.js',
+  '@jpmorganchase/mosaic-theme': '<rootDir>/packages/theme/dist/index.js'
 };
 
 module.exports = {
