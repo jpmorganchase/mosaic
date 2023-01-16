4a47e68d5faa152e017299c6399ba7c506347d61
R662301 <lily.cooksley@jpmchase.com>
2023-01-16 15:49:10 +0000

    regenerate site after generator changes

--- packages/site/mosaic.config.js
+++ packages/site/mosaic.config.js
@@ -12,37 +12,14 @@ const mosaicConfig = require('@jpmorganchase/mosaic-standard-generator/dist/fs.c
 module.exports = deepmerge(mosaicConfig, {
   sources: [
     {
-      modulePath: require.resolve('@jpmorganchase/mosaic-source-local-folder'),
-      namespace: 'local',
+      modulePath: '@jpmorganchase/mosaic-source-local-folder',
+      namespace: 'mosaic',
       options: {
-        rootDir: path.join(process.env.INIT_CWD, 'docs'),
+        rootDir: '/Users/r662301/Documents/workspace/mosaic/docs',
         cache: true,
         prefixDir: 'mosaic',
         extensions: ['.mdx']
       }
     }
-    /*
-    {
-      modulePath: require.resolve('@jpmorganchase/mosaic-source-git-repo'),
-      namespace: 'mosaic', // each site has it's own namespace, think of this as your content's uid
-      options: {
-        // To run locally, enter your credentials to access the BitBucket repo
-        // !! Polite Reminder... do not store credentials in code !!
-        // For final deployments, you could put repo access credentials securely in environment variables provided by Gaia console.
-        // credentials: "{process.env.FID}:{process.env.FID_PERSONAL_ACCESS_TOKEN}",
-        // If running locally
-        // create an environment variable like MOSAIC_DOCS_CLONE_CREDENTIALS to let the user define it via the CLI
-        // export MOSAIC_DOCS_CLONE_CREDENTIALS="<sid>:<Personal Access Token (PAT) provided by your Repo OR password>",
-        credentials: process.env.MOSAIC_DOCS_CLONE_CREDENTIALS,
-        // Add to use a folder prefix
-        // prefixDir: 'cibdat',
-        subfolder: 'docs', // subfolder within your branch containing the docs, typically 'docs'
-        repo: 'INSERT REPO URL', // repo url without any protocol
-        branch: 'INSERT BRANCH NAME', // branch where docs are pulled from
-        extensions: ['.mdx'], // extensions of content which should be pulled
-        remote: 'origin' // what is the shorthand name of the remote repo, typically 'origin'
-      }
-    }
-    */
   ]
 });
