1e80682da1d9385722f355bb84f3967334c06792
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-01-18 23:25:37 +0000

    add the Mosaic site as a sample remote site

--- packages/standard-generator/src/generator.config.js
+++ packages/standard-generator/src/generator.config.js
@@ -73,12 +73,42 @@ module.exports = {
   ],
   sources: [
     {
-      modulePath: '@jpmorganchase/mosaic-source-local-folder',
-      namespace: 'local',
+      /**
+       * Demonstrates a remote source, in this case the Mosaic site
+       * Access from your browser as http://localhost:3000/mosaic
+       */
+      modulePath: '@jpmorganchase/mosaic-source-git-repo',
+      namespace: 'mosaic', // each site has it's own namespace, think of this as your content's uid
       options: {
-        rootDir: 'docs',
-        prefixDir: 'local',
-        extensions: ['.mdx']
+        // To run locally, enter your credentials to access the BitBucket repo
+        // !! Polite Reminder... do not store credentials in code !!
+        // For final deployments, you could put repo access credentials securely in environment variables provided by Gaia console.
+        // credentials: "{process.env.FID}:{process.env.FID_PERSONAL_ACCESS_TOKEN}",
+        // If running locally
+        // create an environment variable like MOSAIC_DOCS_CLONE_CREDENTIALS to let the user define it via the CLI
+        // export MOSAIC_DOCS_CLONE_CREDENTIALS="<sid>:<Personal Access Token (PAT) provided by your Repo OR password>",
+        credentials: process.env.MOSAIC_DOCS_CLONE_CREDENTIALS,
+        prefixDir: 'mosaic', // root path used for namespace
+        cache: true,
+        subfolder: 'docs', // subfolder within your branch containing the docs, typically 'docs'
+        repo: 'https://github.com/jpmorganchase/mosaic.git', // repo url
+        branch: 'main', // branch where docs are pulled from
+        extensions: ['.mdx'], // extensions of content which should be pulled
+        remote: 'origin' // what is the shorthand name of the remote repo, typically 'origin'
+      }
+    },
+    {
+      /**
+       * Demonstrates a local file-system source, in this case a relative path to where the
+       * site was generated.
+       * Access from your browser as http://localhost:3000/local
+       */
+      modulePath: '@jpmorganchase/mosaic-source-local-folder',
+      namespace: 'local', // each site has it's own namespace, think of this as your content's uid
+      options: {
+        rootDir: '../docs', // relative path to content
+        prefixDir: 'local', // root path used for namespace
+        extensions: ['.mdx'] // extensions of content which should be pulled
       }
     }
   ]
