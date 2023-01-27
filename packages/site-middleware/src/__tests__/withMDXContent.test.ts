7469b88b983fe0bd200835aeafa3d88876eea395
mark-tate <143323+mark-tate@users.noreply.github.com>
2023-01-27 12:20:24 +0000

    review comments (#181)
    * review comments
    
    - update `upload-s3-snapshot` to async file read
    - cleanup tests
    - update README.md
    
    * remove unused `serve` snapshot, site not pulls them from local file-system or S3
    based on environment variable `MOSAIC_MODE`
    
    * enable jest-fetch-mock per test
    
    * add missing env vars to tests

--- packages/site-middleware/src/__tests__/withMDXContent.test.ts
+++ packages/site-middleware/src/__tests__/withMDXContent.test.ts
@@ -2,6 +2,8 @@ import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { mockClient } from 'aws-sdk-client-mock';
 import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
 import { Readable } from 'stream';
+import { default as fetchMock, disableFetchMocks, enableFetchMocks } from 'jest-fetch-mock';
+
 const mockFs = require('mock-fs');
 
 import { withMDXContent } from '../withMDXContent';
@@ -11,9 +13,17 @@ jest.mock('../compileMdx.js', () => ({
 }));
 
 describe('GIVEN withMDXContent', () => {
-  describe('GIVEN dynamic Mosaic mode', () => {
+  describe('WHEN snapshot-s3 Mosaic mode is set', () => {
+    let savedEnv = process.env;
     let s3ClientMock;
     beforeAll(() => {
+      process.env = {
+        ...process.env,
+        MOSAIC_S3_BUCKET: 'some-bucket',
+        MOSAIC_S3_REGION: 'some-region',
+        MOSAIC_S3_ACCESS_KEY_ID: 'some-access-key',
+        MOSAIC_S3_SECRET_ACCESS_KEY: 'some-secret-key'
+      };
       s3ClientMock = mockClient(S3Client);
       const stream = new Readable();
       stream.push('my content');
@@ -21,10 +31,11 @@ describe('GIVEN withMDXContent', () => {
       const contentStream = sdkStreamMixin(stream);
       s3ClientMock.on(GetObjectCommand).resolves({ Body: contentStream });
     });
-    afterEach(() => {
+    afterAll(() => {
+      process.env = savedEnv;
       s3ClientMock.reset();
     });
-    test('runs when Mosaic mode is snapshot-s3', async () => {
+    test('THEN a snapshot can be loaded from an S3 bucket', async () => {
       // arrange
       const content = await withMDXContent({
         resolvedUrl: '/mynamespace/mypage.mdx',
@@ -34,43 +45,47 @@ describe('GIVEN withMDXContent', () => {
       expect(content).toEqual({ props: { raw: 'my content', source: 'my content', type: 'mdx' } });
     });
   });
-  describe('GIVEN dynamic Mosaic mode', () => {
-    beforeAll(() => {
-      fetchMock.mockOnce('my content');
-    });
-    test('runs when Mosaic mode is dynamic', async () => {
-      // arrange
-      const content = await withMDXContent({
-        resolvedUrl: '/mynamespace/mypage.mdx',
-        res: { getHeader: () => '/dynamic' }
-      });
-      // assert
-      expect(content).toEqual({ props: { raw: 'my content', source: 'my content', type: 'mdx' } });
-    });
-  });
 
-  describe('GIVEN snapshot-file Mosaic mode', () => {
+  describe('WHEN snapshot-file Mosaic mode is set', () => {
     let savedEnv = process.env;
-    afterEach(() => {
-      mockFs.restore();
-      process.env = savedEnv;
-    });
-    beforeEach(() => {
-      process.env = { MOSAIC_SNAPSHOT_DIR: '/some/snapshots' };
+    beforeAll(() => {
+      process.env = { ...process.env, MOSAIC_SNAPSHOT_DIR: '/some/snapshots' };
       mockFs({
         'some/snapshots/mynamespace/mydir': {
           'mypage.mdx': 'my content'
         }
       });
     });
+    afterAll(() => {
+      mockFs.restore();
+      process.env = savedEnv;
+    });
 
-    test.only('reads snapshots from a local file', async () => {
+    test('THEN a snapshot can be loaded from a local file', async () => {
       // arrange
-      process.env = { MOSAIC_SNAPSHOT_DIR: '/some/snapshots' };
       const content = await withMDXContent({
         resolvedUrl: '/mynamespace/mydir/mypage.mdx',
         res: { getHeader: () => 'snapshot-file' }
       });
+      expect(content).toEqual({ props: { raw: 'my content', source: 'my content', type: 'mdx' } });
+    });
+  });
+
+  describe('WHEN dynamic Mosaic mode is set', () => {
+    beforeAll(() => {
+      enableFetchMocks();
+      fetchMock.mockOnce('my content');
+    });
+    afterAll(() => {
+      disableFetchMocks();
+    });
+    test('THEN content is fetched from the data source', async () => {
+      // arrange
+      const content = await withMDXContent({
+        resolvedUrl: '/mynamespace/mypage.mdx',
+        res: { getHeader: () => '/dynamic' }
+      });
+      // assert
       expect(content).toEqual({ props: { raw: 'my content', source: 'my content', type: 'mdx' } });
     });
   });
--- packages/site-middleware/src/__tests__/withSharedConfig.test.ts
+++ packages/site-middleware/src/__tests__/withSharedConfig.test.ts
@@ -2,13 +2,22 @@ import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { mockClient } from 'aws-sdk-client-mock';
 import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
 import { Readable } from 'stream';
+import { default as fetchMock, disableFetchMocks, enableFetchMocks } from 'jest-fetch-mock';
 const mockFs = require('mock-fs');
 
 import { withSharedConfig } from '../withSharedConfig';
 describe('GIVEN withSharedConfig', () => {
-  describe('GIVEN dynamic Mosaic mode', () => {
+  describe('WHEN snapshot-s3 Mosaic mode is set', () => {
+    let savedEnv = process.env;
     let s3ClientMock;
     beforeAll(() => {
+      process.env = {
+        ...process.env,
+        MOSAIC_S3_BUCKET: 'some-bucket',
+        MOSAIC_S3_REGION: 'some-region',
+        MOSAIC_S3_ACCESS_KEY_ID: 'some-access-key',
+        MOSAIC_S3_SECRET_ACCESS_KEY: 'some-secret-key'
+      };
       s3ClientMock = mockClient(S3Client);
       const stream = new Readable();
       stream.push('{"config": { "someValue": true }}');
@@ -16,10 +25,11 @@ describe('GIVEN withSharedConfig', () => {
       const contentStream = sdkStreamMixin(stream);
       s3ClientMock.on(GetObjectCommand).resolves({ Body: contentStream });
     });
-    afterEach(() => {
+    afterAll(() => {
+      process.env = savedEnv;
       s3ClientMock.reset();
     });
-    test('runs when Mosaic mode is snapshot-s3', async () => {
+    test('THEN shared-config can be loaded from an S3 bucket', async () => {
       // arrange
       const content = await withSharedConfig({
         resolvedUrl: '/mynamespace/mypage.mdx',
@@ -29,28 +39,10 @@ describe('GIVEN withSharedConfig', () => {
       expect(content).toEqual({ props: { sharedConfig: { someValue: true } } });
     });
   });
-  describe('GIVEN dynamic Mosaic mode', () => {
-    beforeAll(() => {
-      fetchMock.mockOnce('{"config": { "someValue": true }}');
-    });
-    test('runs when Mosaic mode is dynamic', async () => {
-      // arrange
-      const content = await withSharedConfig({
-        resolvedUrl: '/mynamespace/mypage.mdx',
-        res: { getHeader: () => '/dynamic' }
-      });
-      // assert
-      expect(content).toEqual({ props: { sharedConfig: { someValue: true } } });
-    });
-  });
 
-  describe('GIVEN snapshot-file Mosaic mode', () => {
+  describe('WHEN snapshot-file Mosaic mode is set', () => {
     let savedEnv = process.env;
-    afterEach(() => {
-      mockFs.restore();
-      process.env = savedEnv;
-    });
-    beforeEach(() => {
+    beforeAll(() => {
       process.env = { MOSAIC_SNAPSHOT_DIR: '/some/snapshots' };
       mockFs({
         'some/snapshots/mynamespace/mydir': {
@@ -58,14 +50,37 @@ describe('GIVEN withSharedConfig', () => {
         }
       });
     });
+    afterAll(() => {
+      mockFs.restore();
+      process.env = savedEnv;
+    });
 
-    test('reads snapshots from a local file', async () => {
+    test('THEN reads shared-config from a local file', async () => {
       // arrange
       process.env = { MOSAIC_SNAPSHOT_DIR: '/some/snapshots' };
       const content = await withSharedConfig({
         resolvedUrl: '/mynamespace/mydir/mypage.mdx',
         res: { getHeader: () => 'snapshot-file' }
       });
+      expect(content).toEqual({ props: { sharedConfig: { someValue: true } } });
+    });
+  });
+
+  describe('WHEN dynamic Mosaic mode is set', () => {
+    beforeAll(() => {
+      enableFetchMocks();
+      fetchMock.mockOnce('{"config": { "someValue": true }}');
+    });
+    afterAll(() => {
+      disableFetchMocks();
+    });
+    test('THEN shared-config is fetched from the data source', async () => {
+      // arrange
+      const content = await withSharedConfig({
+        resolvedUrl: '/mynamespace/mypage.mdx',
+        res: { getHeader: () => '/dynamic' }
+      });
+      // assert
       expect(content).toEqual({ props: { sharedConfig: { someValue: true } } });
     });
   });
