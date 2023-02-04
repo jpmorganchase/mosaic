3d5447217f860d65ad212ed400b96bb9b792e5bf
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-02-04 16:54:28 +0000

    add missing layouts to schema

--- packages/schemas/src/sidebarDataLayoutSchema.ts
+++ packages/schemas/src/sidebarDataLayoutSchema.ts
@@ -1,9 +1,16 @@
 import { z } from 'zod';
 
 export const sidebarDataLayoutSchema = z.union([
+  z.literal('Landing'),
+  z.literal('DetailContentOnly'),
   z.literal('DetailHighlight'),
   z.literal('DetailOverview'),
   z.literal('DetailTechnical'),
+  z.literal('FullWidth'),
   z.literal('NewsLetter'),
+  z.literal('ProductDiscover'),
+  z.literal('ProductPreview'),
+  z.literal('PythonDoc'),
+  z.literal('Typedoc'),
   z.string().endsWith('Doc')
 ]);
