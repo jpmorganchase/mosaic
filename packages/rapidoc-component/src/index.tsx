97fad4eae6a55550368782982977c776a1df283f
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-02-13 23:27:07 +0000

    fix naming of component

--- packages/rapidoc-component/src/index.tsx
+++ packages/rapidoc-component/src/index.tsx
@@ -2,9 +2,9 @@ import React, { lazy } from 'react';
 
 import { IsomorphicSuspense } from './IsomorphicSuspense';
 
-const LazySwagger = lazy(() => import('./Rapidoc'));
+const LazyRapidoc = lazy(() => import('./Rapidoc'));
 export const Rapidoc: React.FC<React.PropsWithChildren<any>> = props => (
   <IsomorphicSuspense fallback={<span>Loading Rapidoc component...</span>}>
-    <LazySwagger {...props} />
+    <LazyRapidoc {...props} />
   </IsomorphicSuspense>
 );
