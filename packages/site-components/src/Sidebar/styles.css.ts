4f8c22972ac72844ad8e1912c63d0029742fb251
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-02-16 00:37:33 +0000

    removed action1 from Sidebar styles and inherit the default page style

--- packages/site-components/src/Sidebar/styles.css.ts
+++ packages/site-components/src/Sidebar/styles.css.ts
@@ -1,12 +1,11 @@
 import { style } from '@vanilla-extract/css';
-import { sidebar, action } from '@jpmorganchase/mosaic-theme';
+import { sidebar } from '@jpmorganchase/mosaic-theme';
 
 export default {
   root: style([
     {
       zIndex: 3
-    },
-    action({ variant: 'action1' })
+    }
   ]),
   left: sidebar({ side: 'left' }),
   right: sidebar({ side: 'right' }),
