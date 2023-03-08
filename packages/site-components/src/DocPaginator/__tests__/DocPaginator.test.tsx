46d7f0b50ea666f52175f86b14bf714dc90cc3e4
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-03-08 22:44:14 +0000

    remove redundant next/previous code from store and replace with navigation data created by NextPrevPagePlugin

---
+++ packages/site-components/src/DocPaginator/__tests__/DocPaginator.test.tsx
@@ -0,0 +1,53 @@
+import React, { PropsWithChildren } from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvents from '@testing-library/user-event';
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+import NextLink from 'next/link';
+import { LinkProvider } from '@jpmorganchase/mosaic-components';
+
+import { DocPaginator } from '../DocPaginator';
+
+describe.only('GIVEN a DocPaginator', () => {
+  const wrapper: PropsWithChildren<LinkProvider> = ({ children }) => (
+    <LinkProvider value={NextLink}>
+      <MemoryRouterProvider>{children}</MemoryRouterProvider>
+    </LinkProvider>
+  );
+
+  test('can change to the next page', async () => {
+    // arrange
+    const next = {
+      title: 'Some Next Page Title',
+      route: '/some-next-page'
+    };
+    render<any>(<DocPaginator linkSuffix="Test Page" next={next} />, {
+      wrapper
+    });
+    // action
+    await userEvents.click(screen.getByText('Next Test Page'));
+    // assert
+    expect(screen.getByText('Some Next Page Title')).toBeInTheDocument();
+    expect(mockRouter).toMatchObject({
+      pathname: '/some-next-page'
+    });
+  });
+
+  test('can change to the previous page', async () => {
+    // arrange
+    const prev = {
+      title: 'Some Previous Page Title',
+      route: '/some-previous-page'
+    };
+    render<any>(<DocPaginator linkSuffix="Test Page" prev={prev} />, {
+      wrapper
+    });
+    // action
+    await userEvents.click(screen.getByText('Previous Test Page'));
+    // assert
+    expect(screen.getByText('Some Previous Page Title')).toBeInTheDocument();
+    expect(mockRouter).toMatchObject({
+      pathname: '/some-previous-page'
+    });
+  });
+});
