df96ecd732f74a74a52b77e024255e7dbeefabc8
Davie <reid5587@gmail.com>
2023-03-24 16:02:08 +0000

    test: add e2e test for `ProductPreview` layout

---
+++ packages/site/e2e/layout.productPreview.test.ts
@@ -0,0 +1,7 @@
+import { test } from '@playwright/test';
+
+test('take a screenshot', async ({ page }) => {
+  await page.goto('/mosaic/test/layouts/product-preview');
+  await page.evaluate(() => document.fonts.ready);
+  await page.screenshot({ path: './e2e/screenshots/layout.product-preview.png', fullPage: true });
+});
