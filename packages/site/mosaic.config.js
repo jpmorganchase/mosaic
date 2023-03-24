dff59b3910ee1ee67c1d0476fb06eb3da032d837
Davie <reid5587@gmail.com>
2023-03-24 15:33:59 +0000

    use mosaic namespace

--- packages/site/mosaic.config.js
+++ packages/site/mosaic.config.js
@@ -1,13 +1,6 @@
 const deepmerge = require('deepmerge');
 const mosaicConfig = require('@jpmorganchase/mosaic-standard-generator/dist/fs.config.js');
 
-/** Enhance/modify your Mosaic core fs
- * pageExtensions: supported file extensions which can be stored in the Virtual File System (VFS) created by Core FS
- * ignorePages: list of files which will be ignored from pulled content,
- * serialisers: define serialisers and deserialisers for the supported file extensions
- * plugins: <...plugin definitions>
- * sources: <...source definitions>
- */
 module.exports = deepmerge(mosaicConfig, {
   deployment: { mode: 'snapshot-file', platform: 'vercel' },
   plugins: [
@@ -25,15 +18,15 @@ module.exports = deepmerge(mosaicConfig, {
     /**
      * Demonstrates a local file-system source, in this case a relative path to where the
      * site was generated.
-     * Access from your browser as http://localhost:3000/local
+     * Access from your browser as http://localhost:3000/mosaic
      */
     {
       disabled: process.env.NODE_ENV !== 'development',
       modulePath: '@jpmorganchase/mosaic-source-local-folder',
-      namespace: 'local', // each site has it's own namespace, think of this as your content's uid
+      namespace: 'mosaic', // each site has it's own namespace, think of this as your content's uid
       options: {
         rootDir: '../../docs', // relative path to content
-        prefixDir: 'local', // root path used for namespace
+        prefixDir: 'mosaic', // root path used for namespace
         extensions: ['.mdx'] // extensions of content which should be pulled
       }
     }
