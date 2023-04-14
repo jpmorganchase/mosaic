b786033367480c9664fc279deaf5380f22044321
Davie <reid5587@gmail.com>
2023-04-14 16:38:40 +0100

    feat: containerize CLI

---
+++ packages/cli/mosaic.config.mjs
@@ -0,0 +1,27 @@
+import deepmerge from 'deepmerge';
+import mosaicConfig from '@jpmorganchase/mosaic-standard-generator/dist/fs.config.js';
+
+const siteConfig = {
+  ...mosaicConfig,
+  plugins: [
+    ...mosaicConfig.plugins,
+    {
+      modulePath: '@jpmorganchase/mosaic-plugins/SidebarPlugin',
+      options: { rootDirGlob: '*/*' }
+    }
+  ]
+};
+
+export default deepmerge(siteConfig, {
+  sources: [
+    {
+      modulePath: '@jpmorganchase/mosaic-source-local-folder',
+      namespace: 'mosaic', // each site has it's own namespace, think of this as your content's uid
+      options: {
+        rootDir: './docs', // relative path to content
+        prefixDir: 'mosaic', // root path used for namespace
+        extensions: ['.mdx'] // extensions of content which should be pulled
+      }
+    }
+  ]
+});
