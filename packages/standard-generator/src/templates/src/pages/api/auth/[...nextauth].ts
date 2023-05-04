72488960f71244d3138fc52c47881fb631b7777e
Davie <reid5587@gmail.com>
2023-05-04 12:39:32 +0100

    feat: upgrade next-auth

--- packages/standard-generator/src/templates/src/pages/api/auth/[...nextauth].ts
+++ packages/standard-generator/src/templates/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
