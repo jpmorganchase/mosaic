4df77ec05331125681c45c7e6e811e9d6dd1be65
Davie <reid5587@gmail.com>
2023-06-15 10:49:40 +0100

    feat: add support for refs to `Button`

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -1,23 +1,25 @@
-import React, { FC } from 'react';
+import React, { type Ref, type ReactNode, forwardRef } from 'react';
 import classnames from 'clsx';
 import { Button as SaltButton, ButtonProps as SaltButtonProps } from '@salt-ds/core';
 import { button as buttonStyles } from '@jpmorganchase/mosaic-theme';
 
 export interface ButtonProps extends Omit<SaltButtonProps, 'variant'> {
   variant?: 'regular' | 'secondary' | 'cta';
+  children?: ReactNode;
 }
 
-export const Button: FC<React.PropsWithChildren<ButtonProps>> = ({
-  children,
-  className,
-  variant = 'regular',
-  ...rest
-}) => (
-  <SaltButton
-    className={classnames(buttonStyles({ variant }), className)}
-    variant={variant === 'regular' ? 'primary' : variant}
-    {...rest}
-  >
-    {children}
-  </SaltButton>
+export const Button = forwardRef(
+  (
+    { children, className, variant = 'regular', ...rest }: ButtonProps,
+    ref: Ref<HTMLButtonElement>
+  ) => (
+    <SaltButton
+      ref={ref}
+      className={classnames(buttonStyles({ variant }), className)}
+      variant={variant === 'regular' ? 'primary' : variant}
+      {...rest}
+    >
+      {children}
+    </SaltButton>
+  )
 );
