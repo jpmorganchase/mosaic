480d3cd779d8758a7925164688042a47b452b960
Davie <reid5587@gmail.com>
2023-06-15 12:02:15 +0100

    fix: adjust active toolbar button styling

--- packages/content-editor-plugin/src/components/Toolbar/ToolbarButton.css.ts
+++ packages/content-editor-plugin/src/components/Toolbar/ToolbarButton.css.ts
@@ -1,12 +1,10 @@
 import { style } from '@vanilla-extract/css';
-import { primaryColor, vars } from '@jpmorganchase/mosaic-theme';
+import { vars } from '@jpmorganchase/mosaic-theme';
 
 export default {
-  active: primaryColor({ variant: 'regular' }),
   root: style({
     width: '28px',
     height: '28px',
-    padding: vars.space.horizontal.none,
-    color: vars.color.dark.neutral.foreground.low
+    padding: vars.space.horizontal.none
   })
 };
