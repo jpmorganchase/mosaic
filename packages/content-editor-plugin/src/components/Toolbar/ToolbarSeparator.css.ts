f2e663ede4d7c9679819142c4e40530053e4e6e7
Davie <reid5587@gmail.com>
2023-06-15 11:13:36 +0100

    fix: adjust toolbar separator color

--- packages/content-editor-plugin/src/components/Toolbar/ToolbarSeparator.css.ts
+++ packages/content-editor-plugin/src/components/Toolbar/ToolbarSeparator.css.ts
@@ -1,19 +1,29 @@
 import { style } from '@vanilla-extract/css';
-import { vars } from '@jpmorganchase/mosaic-theme';
+import { vars, beforeElementColorSprinkles } from '@jpmorganchase/mosaic-theme';
+
+const beforeElementBackground = beforeElementColorSprinkles({
+  backgroundColor: {
+    lightMode: vars.color.dark.neutral.foreground.low,
+    darkMode: vars.color.light.neutral.foreground.low
+  }
+});
 
 export default {
-  root: style({
-    marginLeft: vars.space.horizontal.x2,
-    marginRight: vars.space.horizontal.x1,
-    selectors: {
-      '&:before': {
-        content: '""',
-        position: 'absolute',
-        background: vars.color.dark.neutral.foreground.low,
-        top: '10px',
-        width: '1px',
-        height: vars.space.vertical.x6
+  root: style([
+    beforeElementBackground,
+    style({
+      marginLeft: vars.space.horizontal.x2,
+      marginRight: vars.space.horizontal.x1,
+      selectors: {
+        '&:before': {
+          content: '""',
+          position: 'absolute',
+          background: 'inherit',
+          top: '10px',
+          width: '1px',
+          height: vars.space.vertical.x6
+        }
       }
-    }
-  })
+    })
+  ])
 };
