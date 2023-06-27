c1c53c26aac005dd60cd8d7b0189268744fba834
Davie <reid5587@gmail.com>
2023-06-27 13:54:03 +0100

    refactor: remove salt toolbar from editor controls

--- packages/content-editor-plugin/src/components/EditorControls.tsx
+++ packages/content-editor-plugin/src/components/EditorControls.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import { Icon } from '@jpmorganchase/mosaic-components';
-import { Toolbar as SaltToolbar, Tooltray, ToolbarButton } from '@salt-ds/lab';
+import { BaseToolbar as Toolbar } from './BaseToolbar/BaseToolbar';
+import { BaseTooltray as Tooltray } from './BaseTooltray/BaseTooltray';
+import { ToolbarButton } from './Toolbar/ToolbarButton';
 
 import { default as useContentEditor } from '../store';
 import styles from './EditorControls.css';
 
-export const EditorControls = ({ isLoggedIn = false }) => {
+export interface EditorControlsProps {
+  enabled?: boolean;
+}
+
+export const EditorControls = ({ enabled = false }: EditorControlsProps) => {
   const { pageState, startEditing, stopEditing } = useContentEditor();
   const isEditing = pageState === 'EDIT';
 
   const handleClick = () => (!isEditing ? startEditing() : stopEditing());
 
-  const loggedInLabel = isEditing ? 'Cancel Editing' : 'Edit Page';
-  const overflowLabel = !isLoggedIn ? 'Login Required to Edit' : loggedInLabel;
+  const enabledLabel = isEditing ? 'Cancel Editing' : 'Edit Page';
+  const overflowLabel = !enabled ? 'Login Required to Edit' : enabledLabel;
 
   return (
-    <SaltToolbar aria-label="editor-controls" className={styles.root}>
+    <Toolbar aria-label="editor-controls" className={styles.root}>
       <Tooltray aria-label="page editor controls tooltray">
         <ToolbarButton
           aria-label={isEditing ? 'cancel editing' : 'start editing'}
           onClick={handleClick}
-          disabled={!isLoggedIn}
+          disabled={!enabled}
           label={overflowLabel}
-          style={{ lineHeight: 'normal' }}
         >
           <Icon name={isEditing ? 'delete' : 'edit'} />
         </ToolbarButton>
       </Tooltray>
-    </SaltToolbar>
+    </Toolbar>
   );
 };
