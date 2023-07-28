3f70f4ed1c6cea58fcad5a18c73bb48e925993c8
Davie <reid5587@gmail.com>
2023-07-28 12:39:20 +0100

    feat: add schedules to remote sources

--- packages/source-http/src/index.ts
+++ packages/source-http/src/index.ts
@@ -3,12 +3,13 @@ import { switchMap, map } from 'rxjs/operators';
 import { z } from 'zod';
 import type { Page, Source } from '@jpmorganchase/mosaic-types';
 import { fromHttpRequest, isErrorResponse } from '@jpmorganchase/mosaic-from-http-request';
+import { sourceScheduleSchema, validateMosaicSchema } from '@jpmorganchase/mosaic-schemas';
+
 import { fromDynamicImport } from './fromDynamicImport.js';
 
 export const schema = z.object({
-  checkIntervalMins: z.number().optional().default(5),
+  schedule: sourceScheduleSchema.optional(),
   endpoints: z.array(z.string()).nonempty('Please provide a URL'),
-  initialDelayMs: z.number().optional().default(1000),
   prefixDir: z.string({ required_error: 'Please provide a prefixDir' }),
   transformResponseToPagesModulePath: z.string({
     description: `The path to a module that exports a function to transform request responses into Pages. The transformer must bea default export.
@@ -20,20 +21,16 @@ export const schema = z.object({
 export type HttpSourceOptions = z.infer<typeof schema>;
 
 const HttpSource: Source<HttpSourceOptions> = {
-  create(options): Observable<Page[]> {
-    const {
-      checkIntervalMins,
-      initialDelayMs,
-      endpoints,
-      prefixDir,
-      transformResponseToPagesModulePath
-    } = schema.parse(options);
-
-    const delayMs = checkIntervalMins * 60000;
+  create(options, { schedule }): Observable<Page[]> {
+    const { endpoints, prefixDir, transformResponseToPagesModulePath } = validateMosaicSchema(
+      schema,
+      options
+    );
+    const delayMs = schedule.checkIntervalMins * 60000;
 
     return fromDynamicImport(transformResponseToPagesModulePath).pipe(
       switchMap(({ transformer, requestConfig }) =>
-        timer(initialDelayMs, delayMs).pipe(
+        timer(schedule.initialDelayMs, delayMs).pipe(
           switchMap(() => {
             const requests = endpoints.map(endpoint => {
               const request = requestConfig ? new Request(endpoint, requestConfig) : endpoint;
