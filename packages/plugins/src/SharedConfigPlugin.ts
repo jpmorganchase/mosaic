cf852bdaba36c9e6d87c5c8c1032dcf1ce3652a9
Davie <reid5587@gmail.com>
2023-08-07 16:21:48 +0100

    fix: ensure shared configs are merged

--- packages/plugins/src/SharedConfigPlugin.ts
+++ packages/plugins/src/SharedConfigPlugin.ts
@@ -1,6 +1,14 @@
 import type { Page, Plugin as PluginType } from '@jpmorganchase/mosaic-types';
-import { flatten } from 'lodash-es';
+import { flatten, escapeRegExp } from 'lodash-es';
 import path from 'path';
+import deepmerge from 'deepmerge';
+
+function createPageTest(ignorePages, pageExtensions) {
+  const extTest = new RegExp(`${pageExtensions.map(escapeRegExp).join('|')}$`);
+  const ignoreTest = new RegExp(`${ignorePages.map(escapeRegExp).join('|')}$`);
+  return file =>
+    !ignoreTest.test(file) && extTest.test(file) && !path.basename(file).startsWith('.');
+}
 
 function createFileGlob(url, pageExtensions) {
   if (pageExtensions.length === 1) {
@@ -15,15 +23,18 @@ function createFileGlob(url, pageExtensions) {
  * If it's not contained, the first component of the resulting path will be .., so that's what we check for
  */
 function isWithin(outer, inner) {
-  const rel = path.posix.relative(outer, inner);
+  const outerParentDir = path.posix.resolve(path.posix.dirname(outer), '../');
+  const innerParentDir = path.posix.resolve(path.posix.dirname(inner), '../');
+  const rel = path.posix.relative(outerParentDir, innerParentDir);
   return !rel.startsWith('../') && rel !== '..';
 }
 
-interface SharedConfigPluginPage extends Page {
+export interface SharedConfigPluginPage extends Page {
   sharedConfig?: string;
+  frameOverrides?: any;
 }
 
-interface SharedConfigPluginOptions {
+export interface SharedConfigPluginOptions {
   filename: string;
 }
 
@@ -32,12 +43,41 @@ interface SharedConfigPluginOptions {
  * It then exports a JSON file (name: `options.filename`) into each directory with the merged config for that level
  */
 const SharedConfigPlugin: PluginType<SharedConfigPluginPage, SharedConfigPluginOptions> = {
+  async $afterSource(pages, { ignorePages, pageExtensions }) {
+    const isNonHiddenPage = createPageTest(ignorePages, pageExtensions);
+    let finalSharedConfig;
+
+    const indexPagesWithSharedConfig = pages.filter(
+      page =>
+        path.posix.basename(page.fullPath, path.posix.extname(page.fullPath)) === 'index' &&
+        page.sharedConfig !== undefined &&
+        isNonHiddenPage(page.fullPath)
+    );
+
+    for (const page of indexPagesWithSharedConfig) {
+      if (finalSharedConfig === undefined) {
+        // first shared config we have found so seed the finalSharedConfig
+        finalSharedConfig = page.sharedConfig;
+      } else {
+        for (let i = 1; i < indexPagesWithSharedConfig.length; i++) {
+          if (isWithin(indexPagesWithSharedConfig[i].fullPath, page.fullPath)) {
+            // found another shared config so merge and apply to the page
+            finalSharedConfig = deepmerge(finalSharedConfig, page.sharedConfig);
+            page.sharedConfig = finalSharedConfig;
+            page.frameOverrides = { $ref: '#/sharedConfig' };
+          }
+        }
+      }
+    }
+    return pages;
+  },
+
   async $beforeSend(
     mutableFilesystem,
     { config, serialiser, ignorePages, pageExtensions },
     options
   ) {
-    const pagePaths = await mutableFilesystem.promises.glob(
+    const indexPagePaths = await mutableFilesystem.promises.glob(
       createFileGlob('**/index', pageExtensions),
       {
         ignore: [options.filename, ...flatten(ignorePages.map(ignore => [ignore, `**/${ignore}`]))],
@@ -45,38 +85,38 @@ const SharedConfigPlugin: PluginType<SharedConfigPluginPage, SharedConfigPluginO
       }
     );
 
-    const sharedConfigFiles = [];
-    const pagesWithoutConfig: string[] = [];
+    const sharedConfigFiles: string[] = [];
+    const indexPagesWithoutConfig: string[] = [];
 
-    for (const pagePath of pagePaths) {
+    for (const indexPagePath of indexPagePaths) {
       const sharedConfigFile = path.posix.join(
-        path.posix.dirname(String(pagePath)),
+        path.posix.dirname(String(indexPagePath)),
         options.filename
       );
 
       const page = await serialiser.deserialise(
-        String(pagePath),
-        await mutableFilesystem.promises.readFile(String(pagePath))
+        String(indexPagePath),
+        await mutableFilesystem.promises.readFile(String(indexPagePath))
       );
+
       if (page.sharedConfig) {
-        config.setRef(sharedConfigFile, ['config', '$ref'], `${String(pagePath)}#/sharedConfig`);
+        config.setRef(
+          sharedConfigFile,
+          ['config', '$ref'],
+          `${String(indexPagePath)}#/sharedConfig`
+        );
         await mutableFilesystem.promises.writeFile(sharedConfigFile, '{}');
         sharedConfigFiles.push(sharedConfigFile);
       } else {
-        pagesWithoutConfig.push(page.fullPath);
+        indexPagesWithoutConfig.push(page.fullPath);
       }
     }
 
+    // apply closest shared config
     let closestSharedConfigIndex = 0;
-    for (const pagePath of pagesWithoutConfig) {
+    for (const pagePath of indexPagesWithoutConfig) {
       for (let i = 0; i < sharedConfigFiles.length; i++) {
-        const sharedConfigBaseDir = path.posix.resolve(
-          path.posix.dirname(sharedConfigFiles[i]),
-          '../'
-        );
-        const pageBaseDir = path.posix.resolve(path.posix.dirname(String(pagePath)), '../');
-
-        if (isWithin(sharedConfigBaseDir, pageBaseDir)) {
+        if (isWithin(sharedConfigFiles[i], pagePath)) {
           closestSharedConfigIndex = i;
         }
       }
---
+++ packages/plugins/src/__tests__/SharedConfigPlugin.test.tsx
@@ -0,0 +1,195 @@
+import { Page } from '@jpmorganchase/mosaic-types';
+import SharedConfigPlugin from '../SharedConfigPlugin';
+
+type SharedConfigPage = Page & { sharedConfig?: any };
+/**
+ * 
+ * Folder Hierarchy
+
+
+Folder A/
+├─ Subfolder A/
+│  ├─ index.mdx
+│  ├─ Page A.mdx
+├─ Subfolder B/
+│  ├─ Subfolder C/
+│  │  ├─ Subfolder D/
+│  │  │  ├─ index.mdx
+│  ├─ index.mdx
+index.mdx
+Page A.mdx
+Page B.mdx
+
+ * 
+ */
+const pages: SharedConfigPage[] = [
+  {
+    fullPath: '/FolderA/index.mdx',
+    route: 'route/folderA/index',
+    title: 'Folder A Index',
+    sharedConfig: {
+      shared1: 'shared 1',
+      shared2: 'shared 2'
+    }
+  },
+  {
+    fullPath: '/FolderA/pageA.mdx',
+    route: 'route/folderA/pageA',
+    title: 'Folder A Page A'
+  },
+  {
+    fullPath: '/FolderA/pageB.mdx',
+    route: 'route/folderA/pageB',
+    title: 'Folder A Page B'
+  },
+  {
+    fullPath: '/FolderA/SubfolderA/index.mdx',
+    route: 'route/folderA/subfolderA/index',
+    title: 'Subfolder A Index',
+    sharedConfig: {
+      shared2: 'overwritten 2',
+      shared3: 'shared 3'
+    }
+  },
+  {
+    fullPath: '/FolderA/SubfolderA/PageA.mdx',
+    route: 'route/folderA/subfolderA/pageA',
+    title: 'Subfolder A Page A'
+  },
+  {
+    fullPath: '/FolderA/SubfolderB/index.mdx',
+    route: 'route/folderA/subfolderB/index',
+    title: 'Subfolder B Index',
+    sharedConfig: {
+      shared2: 'overwritten 3'
+    }
+  },
+  {
+    fullPath: '/FolderA/SubfolderB/SubfolderC/SubfolderD/index.mdx',
+    route: 'route/folderA/subfolderB/subfolderC/subfolderD/index',
+    title: 'Subfolder D Index'
+  }
+];
+
+let setAliasesMock = jest.fn();
+let setRefMock = jest.fn();
+let writeFileMock = jest.fn();
+const volume = {
+  promises: {
+    exists: jest.fn().mockResolvedValue(true),
+    glob: jest.fn().mockResolvedValue([
+      pages[0].fullPath, // Folder A Index
+      pages[3].fullPath, // Subfolder A Index
+      pages[5].fullPath, // Subfolder B Index
+      pages[6].fullPath // Subfolder D Index
+    ]),
+    mkdir: jest.fn(),
+    readdir: jest.fn(),
+    readFile: jest.fn(value => Promise.resolve(pages.find(page => page.fullPath === value))),
+    realpath: jest.fn(),
+    stat: jest.fn(),
+    symlink: jest.fn(),
+    unlink: jest.fn(),
+    writeFile: writeFileMock
+  }
+};
+
+describe('GIVEN the SharedConfigPlugin', () => {
+  test('THEN it should use the `$afterSource` lifecycle event', () => {
+    expect(SharedConfigPlugin).toHaveProperty('$afterSource');
+  });
+
+  test('THEN it should use the `$beforeSend` lifecycle event', () => {
+    expect(SharedConfigPlugin).toHaveProperty('$beforeSend');
+  });
+
+  describe('WHEN `$afterSource` is called', () => {
+    let updatedPages: SharedConfigPage[] = [];
+    beforeEach(async () => {
+      const $afterSource = SharedConfigPlugin.$afterSource;
+      // @ts-ignore
+      updatedPages =
+        (await $afterSource?.(
+          pages,
+          {
+            pageExtensions: ['.mdx'],
+            ignorePages: ['shared-config.json']
+          },
+          { filename: 'shared-config.json' }
+        )) || [];
+    });
+    test('THEN shared config is untouched when there is no parent config', () => {
+      expect(updatedPages[0].sharedConfig).toEqual({
+        shared1: 'shared 1',
+        shared2: 'shared 2'
+      });
+    });
+    test('THEN properties only in the parent shared config are copied to the child shared config', () => {
+      expect(updatedPages[3].sharedConfig.shared1).toEqual('shared 1');
+    });
+    test('THEN child shared config properties will overwrite parent properties with the same name', () => {
+      expect(updatedPages[3].sharedConfig.shared2).toEqual('overwritten 2');
+    });
+    test('THEN properties only present in the child shared config are copied', () => {
+      expect(updatedPages[3].sharedConfig.shared3).toEqual('shared 3');
+    });
+  });
+
+  describe('WHEN `$beforeSend` is called', () => {
+    beforeEach(async () => {
+      const $beforeSend = SharedConfigPlugin.$beforeSend;
+      // @ts-ignore
+      (await $beforeSend?.(
+        volume,
+        {
+          config: {
+            setRef: setRefMock,
+            setAliases: setAliasesMock
+          },
+          serialiser: {
+            deserialise: jest.fn().mockImplementation((_path, value) => Promise.resolve(value))
+          },
+          ignorePages: [],
+          pageExtensions: []
+        },
+        { filename: 'test-shared-config.json' }
+      )) || [];
+    });
+
+    afterEach(() => {
+      setAliasesMock.mockClear();
+      setRefMock.mockClear();
+    });
+
+    test('THEN refs to the shared config files are created', () => {
+      expect(setRefMock.mock.calls.length).toEqual(3);
+      expect(setRefMock.mock.calls[0][0]).toEqual('/FolderA/test-shared-config.json');
+      expect(setRefMock.mock.calls[0][1]).toEqual(['config', '$ref']);
+      expect(setRefMock.mock.calls[0][2]).toEqual('/FolderA/index.mdx#/sharedConfig');
+
+      expect(setRefMock.mock.calls[1][0]).toEqual('/FolderA/SubfolderA/test-shared-config.json');
+      expect(setRefMock.mock.calls[1][1]).toEqual(['config', '$ref']);
+      expect(setRefMock.mock.calls[1][2]).toEqual('/FolderA/SubfolderA/index.mdx#/sharedConfig');
+
+      expect(setRefMock.mock.calls[2][0]).toEqual('/FolderA/SubfolderB/test-shared-config.json');
+      expect(setRefMock.mock.calls[2][1]).toEqual(['config', '$ref']);
+      expect(setRefMock.mock.calls[2][2]).toEqual('/FolderA/SubfolderB/index.mdx#/sharedConfig');
+    });
+
+    test('THEN aliases to the created shared config files are created for index pages that do **not** have their own shared config', () => {
+      expect(setAliasesMock.mock.calls[0][0]).toEqual(
+        '/FolderA/SubfolderB/test-shared-config.json'
+      );
+    });
+
+    test('THEN aliases point to the closest shared config in the folder hierarchy', () => {
+      expect(setAliasesMock.mock.calls.length).toEqual(1);
+      expect(setAliasesMock.mock.calls[0][0]).toEqual(
+        '/FolderA/SubfolderB/test-shared-config.json'
+      );
+      expect(setAliasesMock.mock.calls[0][0]).toEqual(
+        '/FolderA/SubfolderB/test-shared-config.json'
+      );
+    });
+  });
+});
