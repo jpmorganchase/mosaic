625e8678768df08cd3870037d68350f9b51e7760
Davie <reid5587@gmail.com>
2023-08-23 17:03:07 +0100

    feat: throw `PluginError` when `BreadcrumbsPlugin` fails

--- packages/plugins/src/BreadcrumbsPlugin.ts
+++ packages/plugins/src/BreadcrumbsPlugin.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import type { Page, Plugin as PluginType } from '@jpmorganchase/mosaic-types';
 import { breadcrumbsLayoutSchema } from '@jpmorganchase/mosaic-schemas';
+import PluginError from './utils/PluginError.js';
 
 export type Breadcrumb = { label: string; path: string; id: string };
 
@@ -19,33 +20,37 @@ interface BreadcrumbsPluginOptions {
 const BreadcrumbsPlugin: PluginType<BreadcrumbsPluginPage, BreadcrumbsPluginOptions> = {
   async $afterSource(pages, _, options) {
     for (const page of pages) {
-      if (breadcrumbsLayoutSchema.safeParse(page?.layout).success) {
-        const breadcrumbs: Array<Breadcrumb> = [];
-        let currentPage = page;
-        let parentDir = path.posix.normalize(path.posix.dirname(currentPage.fullPath));
-        const maxBreadcrumbCount = page.fullPath.split('/').length;
+      try {
+        if (breadcrumbsLayoutSchema.safeParse(page?.layout).success) {
+          const breadcrumbs: Array<Breadcrumb> = [];
+          let currentPage = page;
+          let parentDir = path.posix.normalize(path.posix.dirname(currentPage.fullPath));
+          const maxBreadcrumbCount = page.fullPath.split('/').length;
 
-        while (currentPage !== undefined && breadcrumbs.length < maxBreadcrumbCount) {
-          breadcrumbs.unshift({
-            label: currentPage.title,
-            path: currentPage.route,
-            id: currentPage.fullPath
-          });
+          while (currentPage !== undefined && breadcrumbs.length < maxBreadcrumbCount) {
+            breadcrumbs.unshift({
+              label: currentPage.title,
+              path: currentPage.route,
+              id: currentPage.fullPath
+            });
 
-          currentPage = pages.find(
-            // eslint-disable-next-line @typescript-eslint/no-loop-func
-            item => item.fullPath === path.posix.join(parentDir, options.indexPageName)
-          );
-          if (currentPage) {
-            parentDir = path.posix.dirname(path.posix.join(String(currentPage.fullPath), '..'));
+            currentPage = pages.find(
+              // eslint-disable-next-line @typescript-eslint/no-loop-func
+              item => item.fullPath === path.posix.join(parentDir, options.indexPageName)
+            );
+            if (currentPage) {
+              parentDir = path.posix.dirname(path.posix.join(String(currentPage.fullPath), '..'));
+            }
+          }
+
+          if (!page.breadcrumbs) {
+            // filter out any duplicate breadcrumbs
+            const map = new Map(breadcrumbs.map(crumb => [crumb.id, crumb]));
+            page.breadcrumbs = [...map.values()];
           }
         }
-
-        if (!page.breadcrumbs) {
-          // filter out any duplicate breadcrumbs
-          const map = new Map(breadcrumbs.map(crumb => [crumb.id, crumb]));
-          page.breadcrumbs = [...map.values()];
-        }
+      } catch (e) {
+        throw new PluginError(e.message, page.fullPath);
       }
     }
 
