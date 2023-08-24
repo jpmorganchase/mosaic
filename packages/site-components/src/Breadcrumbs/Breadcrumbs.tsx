593b7258e253c9c2d67b498cd9b4dd00c637a173
Davie <reid5587@gmail.com>
2023-08-24 16:50:46 +0100

    fix: support nav when collapsed breadcrumb is clicked

--- packages/site-components/src/Breadcrumbs/Breadcrumbs.tsx
+++ packages/site-components/src/Breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { Breadcrumbs as SaltBreadcrumbs } from '@salt-ds/lab';
 import { Breadcrumb } from './Breadcrumb';
 
@@ -16,19 +17,41 @@ export type BreadcrumbsProps = {
 };
 
 export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ breadcrumbs, enabled }) => {
+  const router = useRouter();
   if (!enabled) {
     return null;
   }
+
+  /**
+   * TODO - we need appropriate keyboard nav support as well
+   * The slat cascading menu keyboard nav does not seem to work
+   * so not much point adding support for enter key selection here
+   */
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLDivElement;
+    if (target.classList.contains('saltMenuItem-menuItemText')) {
+      const breadcrumbIndex = breadcrumbs.findIndex(
+        breadcrumb => breadcrumb.label.toLowerCase() === target.textContent?.toLowerCase()
+      );
+
+      if (breadcrumbIndex > -1) {
+        router.push(breadcrumbs[breadcrumbIndex].path);
+      }
+    }
+  };
+
   return (
-    <SaltBreadcrumbs className={styles.root} itemsBeforeCollapse={2} maxItems={5}>
-      {breadcrumbs.map(
-        value =>
-          value && (
-            <Breadcrumb href={value.path} key={`${value.id}`} overflowLabel={value.label}>
-              {value.label}
-            </Breadcrumb>
-          )
-      )}
-    </SaltBreadcrumbs>
+    <div onClick={handleClick}>
+      <SaltBreadcrumbs className={styles.root} itemsBeforeCollapse={2} maxItems={5}>
+        {breadcrumbs.map(
+          value =>
+            value && (
+              <Breadcrumb href={value.path} key={`${value.id}`} overflowLabel={value.label}>
+                {value.label}
+              </Breadcrumb>
+            )
+        )}
+      </SaltBreadcrumbs>
+    </div>
   );
 };
