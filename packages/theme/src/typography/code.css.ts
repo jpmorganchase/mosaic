8a5cbef3cc12c82df9a2e327d992ddd8e8d99853
Davie <reid5587@gmail.com>
2023-08-31 11:10:26 +0100

    fix: adjust styling of inline code blocks

--- packages/theme/src/typography/code.css.ts
+++ packages/theme/src/typography/code.css.ts
@@ -3,6 +3,7 @@ import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 
 import { vars } from '../vars.css';
 import { backgroundColor, foregroundColor } from '../color';
+import { neutralBorder } from '../border';
 
 export const code = recipe({
   variants: {
@@ -11,13 +12,17 @@ export const code = recipe({
       regular: style([
         {
           fontSize: vars.fontSize.s70,
+          paddingLeft: vars.space.horizontal.x2,
+          paddingRight: vars.space.horizontal.x2,
+          whiteSpace: 'nowrap',
           selectors: {
             ['code.&']: {
               fontWeight: vars.fontWeight.light
             }
           }
         },
-        backgroundColor({ variant: 'regular' }),
+        neutralBorder({ variant: 'low', borderWidth: 'thin' }),
+        backgroundColor({ variant: 'emphasis' }),
         foregroundColor({ variant: 'mid' })
       ])
     }
