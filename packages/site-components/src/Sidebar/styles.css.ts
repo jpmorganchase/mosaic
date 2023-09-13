fdbc4bf8409cd46501c76d1e43be5c5b1f0016c7
Davie <reid5587@gmail.com>
2023-09-13 15:08:07 +0100

    fix: prevent sidebars width changing re content size

--- packages/site-components/src/Sidebar/styles.css.ts
+++ packages/site-components/src/Sidebar/styles.css.ts
@@ -4,7 +4,9 @@ import { sidebar } from '@jpmorganchase/mosaic-theme';
 export default {
   root: style([
     {
-      zIndex: 3
+      zIndex: 3,
+      flexGrow: 0,
+      width: '300px'
     },
     sidebar.container
   ]),
