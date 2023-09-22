621ad38acbc113557ea262e1fd1d36d9f60f24ca
Davie <reid5587@gmail.com>
2023-09-22 10:46:14 +0100

    refactor: switch to latest Salt Input and FormField in link editor

--- packages/content-editor-plugin/src/components/Toolbar/InsertLink.tsx
+++ packages/content-editor-plugin/src/components/Toolbar/InsertLink.tsx
@@ -2,14 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { Button, Icon } from '@jpmorganchase/mosaic-components';
 import { string, object } from 'yup';
-import {
-  FormField,
-  Input,
-  ButtonBar,
-  DialogTitle,
-  DialogContent,
-  DialogActions
-} from '@salt-ds/lab';
+import { Input, FormField, FormFieldLabel, FormFieldHelperText } from '@salt-ds/core';
+import { ButtonBar, DialogTitle, DialogContent, DialogActions } from '@salt-ds/lab';
 import { $getSelection, $isRangeSelection } from 'lexical';
 
 import { ToolbarButton } from './ToolbarButton';
@@ -20,7 +14,7 @@ import styles from './InsertLink.css';
 
 const validationSchema = object({
   url: string().required('Url is required'),
-  text: string().required('Text is required').max(10, 'Text must be fewer than 100 characters')
+  text: string().required('Text is required').max(100, 'Text must be fewer than 100 characters')
 });
 
 export const InsertLinkButton = () => {
@@ -37,7 +31,8 @@ export const InsertLinkButton = () => {
 };
 
 const initialState = {
-  url: 'https://'
+  url: 'https://',
+  text: ''
 };
 
 type FormValueState =
@@ -78,7 +73,7 @@ export const InsertLinkDialog = () => {
         const selection = $getSelection();
         if ($isRangeSelection(selection)) {
           const textContent = selection.getTextContent();
-          setValues(values => ({ ...values, text: textContent }));
+          setValues(prevState => ({ ...prevState, text: textContent }));
         }
       }
     });
@@ -95,8 +90,8 @@ export const InsertLinkDialog = () => {
     setErrors(newErrors);
   };
 
-  const handleChange = (event, value) => {
-    const { name } = event.target;
+  const handleChange = event => {
+    const { name, value } = event.target;
     const newValues = { ...values, [name]: value };
     validationSchema.validateAt(name, newValues, { abortEarly: false }).then(() => {
       setErrors({ ...errors, [name]: undefined });
@@ -104,8 +99,7 @@ export const InsertLinkDialog = () => {
     setValues(newValues);
   };
 
-  const handleSubmit = async event => {
-    event.preventDefault();
+  const handleSubmit = async () => {
     validationSchema.validate(values, { abortEarly: false }).then(() => {
       const payload: InsertLinkPayload = {
         url: values?.url,
@@ -118,35 +112,29 @@ export const InsertLinkDialog = () => {
 
   return (
     <Dialog onOpenChange={handleOpenChange} open={isInsertingLink}>
-      <form onSubmit={handleSubmit} noValidate>
-        <DialogTitle>Insert Link</DialogTitle>
-        <DialogContent>
-          <div className={styles.fullWidth}>
-            <FormField
-              label="Image URL text"
-              validationStatus={errors?.url ? 'error' : undefined}
-              helperText={errors?.url}
-            >
-              <Input value={values?.url} inputProps={{ name: 'url' }} onChange={handleChange} />
-            </FormField>
-            <FormField
-              label="Link text"
-              validationStatus={errors?.text ? 'error' : undefined}
-              helperText={errors?.text}
-            >
-              <Input value={values?.text} inputProps={{ name: 'text' }} onChange={handleChange} />
-            </FormField>
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <ButtonBar>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button variant="cta" type="submit">
-              Insert
-            </Button>
-          </ButtonBar>
-        </DialogActions>
-      </form>
+      <DialogTitle>Insert Link</DialogTitle>
+      <DialogContent>
+        <div className={styles.fullWidth}>
+          <FormField validationStatus={errors?.url ? 'error' : undefined}>
+            <FormFieldLabel>Image URL text</FormFieldLabel>
+            <Input value={values?.url} inputProps={{ name: 'url' }} onChange={handleChange} />
+            <FormFieldHelperText>{errors?.url}</FormFieldHelperText>
+          </FormField>
+          <FormField validationStatus={errors?.text ? 'error' : undefined}>
+            <FormFieldLabel>Link Text</FormFieldLabel>
+            <Input value={values?.text} inputProps={{ name: 'text' }} onChange={handleChange} />
+            <FormFieldHelperText>{errors?.text}</FormFieldHelperText>
+          </FormField>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <ButtonBar>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button variant="cta" onClick={handleSubmit}>
+            Insert
+          </Button>
+        </ButtonBar>
+      </DialogActions>
     </Dialog>
   );
 };
