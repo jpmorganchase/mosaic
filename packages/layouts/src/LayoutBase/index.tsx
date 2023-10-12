33fab0d5e8cec0b819f88358c09ec34e60ece7cf
Davie <reid5587@gmail.com>
2023-10-12 10:08:03 +0100

    refactor: delete layout provider

--- packages/layouts/src/LayoutBase/index.tsx
+++ packages/layouts/src/LayoutBase/index.tsx
@@ -1,19 +1,7 @@
-import React, { Suspense } from 'react';
-import { Spinner } from '@salt-ds/core';
+import React from 'react';
 import classnames from 'clsx';
-import { SidebarProvider } from '@jpmorganchase/mosaic-site-components';
 
-import { Fade } from '../Fade';
 import styles from './styles.css';
-
-const Loading = () => (
-  <Fade duration={{ enter: 1200, exit: 500 }}>
-    <div className={styles.overlayRoot}>
-      <div className={styles.overlayInner} />
-      <Spinner size="large" />
-    </div>
-  </Fade>
-);
 
 export const LayoutBase = ({
   Header,
@@ -23,17 +11,9 @@ export const LayoutBase = ({
   Header?: React.ReactNode;
   className?: string;
   children?: React.ReactNode;
-}) => {
-  return (
-    <SidebarProvider>
-      <div className={classnames(styles.root, className)}>
-        <header className={styles.header}>{Header}</header>
-        <main className={styles.main}>
-          <Suspense fallback={<Loading />}>
-            <React.Fragment>{children}</React.Fragment>
-          </Suspense>
-        </main>
-      </div>
-    </SidebarProvider>
-  );
-};
+}) => (
+  <div className={classnames(styles.root, className)}>
+    <header className={styles.header}>{Header}</header>
+    <main className={styles.main}>{children}</main>
+  </div>
+);
--- packages/layouts/src/index.ts
+++ packages/layouts/src/index.ts
@@ -1,8 +1,5 @@
-'use client';
 export * from './LayoutBase';
 export * from './LayoutColumns';
 export * from './LayoutFullWidth';
-export * from './LayoutConfigProvider';
-export * from './LayoutProvider';
 
 export * as layouts from './layouts';
