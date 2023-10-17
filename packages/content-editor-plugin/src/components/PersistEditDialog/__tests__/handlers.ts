4edbda8a223bc3b7de32fe06ec6ad1e2af600572
Davie <reid5587@gmail.com>
2023-10-17 09:59:35 +0100

    tests: update PersistDialog tests

--- packages/content-editor-plugin/src/components/PersistEditDialog/__tests__/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { rest } from 'msw';
-
-export const postSaveContentSuccessHandler = rest.post('/api/content/save', (req, res, ctx) =>
-  res(ctx.status(500), ctx.json({ links: { self: [{ href: 'http://pull-request-url' }] } }))
-);
-
-export const postSaveContentFailureHandler = rest.post('/api/content/save', (req, res, ctx) =>
-  res(ctx.status(500))
-);
