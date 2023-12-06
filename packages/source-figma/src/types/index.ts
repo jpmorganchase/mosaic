ed3f0494e04d3050a9c7e96f9c794d1d3b9a5f89
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-12-06 17:02:08 +0000

    feat: add Figma source (UITK-10184)
    A Figma source pulls tagged patterns via Figma's REST API and stores them
    as JSON in the VFS.
    
    We can build up dynamic pages which group/list Figma embeds from this metadata.

---
+++ packages/source-figma/src/types/index.ts
@@ -0,0 +1,46 @@
+import type { Page } from '@jpmorganchase/mosaic-types';
+
+/** Get project response */
+export type ProjectResponseJson = {
+  document: {
+    id: string;
+    name: string;
+    sharedPluginData: Record<string, any>;
+  };
+  name: string;
+  lastModified: string;
+  thumbnailUrl: string;
+  version: string;
+};
+
+export type ProjectFile = {
+  key: string;
+  thumbnail_url: string;
+  last_modified: string;
+  branches?: ProjectFile[];
+};
+
+/** Get project files response */
+export type ProjectFilesResponseJson = {
+  name: string;
+  files: ProjectFile[];
+};
+
+/** Figma page Metadata */
+export type FigmaPageData = {
+  name: string;
+  patternId: string;
+  description: string;
+  embedLink: string;
+  link: string;
+  source: 'FIGMA';
+  tags?: string;
+};
+
+/** Figma page */
+export type FigmaPage = {
+  content: string;
+  description?: string;
+  data: FigmaPageData;
+  tags: string[];
+} & Page;
