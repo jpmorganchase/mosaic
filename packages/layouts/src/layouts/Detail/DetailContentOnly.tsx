cc5e8388d9e4784ea48db7683152da561fa60dd8
Davie <reid5587@gmail.com>
2024-01-05 13:49:19 +0000

    update DetailContentOnly layout and remove components no longer required.

--- packages/layouts/src/layouts/Detail/DetailContentOnly.tsx
+++ packages/layouts/src/layouts/Detail/DetailContentOnly.tsx
@@ -3,13 +3,6 @@ import React from 'react';
 import { LayoutFullWidth } from '../../LayoutFullWidth';
 import type { LayoutProps } from '../../types';
 
-export const DetailContentOnly: React.FC<LayoutProps> = ({
-  children,
-  BreadcrumbsComponent,
-  FooterComponent
-}) => (
-  <LayoutFullWidth Footer={FooterComponent}>
-    <BreadcrumbsComponent />
-    {children}
-  </LayoutFullWidth>
+export const DetailContentOnly: React.FC<LayoutProps> = ({ children, FooterComponent }) => (
+  <LayoutFullWidth Footer={FooterComponent}>{children}</LayoutFullWidth>
 );
