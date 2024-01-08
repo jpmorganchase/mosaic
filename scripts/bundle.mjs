cdec04f8181f2d8341b2e4aacceae1506b97a6a6
Davie <reid5587@gmail.com>
2024-01-08 12:49:25 +0000

    update watch mode in build script

--- scripts/bundle.mjs
+++ scripts/bundle.mjs
@@ -1,4 +1,4 @@
-import esbuild from 'esbuild';
+import * as esbuild from 'esbuild';
 import glob from 'fast-glob';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
@@ -7,61 +7,48 @@ const args = process.argv.slice(2);
 const watchEnabled = args[0] === 'watch';
 const packageName = process.env.npm_package_name;
 
-try {
-  const context = await esbuild.context({
-    entryPoints: glob.sync(['src/**/*.ts?(x)', 'src/*.ts?(x)'], {
-      ignore: ['**/__tests__', 'src/labs']
-    }),
-    loader: {
-      '.jpg': 'dataurl',
-      '.png': 'dataurl',
-      '.svg': 'text'
-    },
-    outdir: './dist',
-    bundle: true,
-    sourcemap: false,
-    splitting: true,
-    minify: true,
-    format: 'esm',
-    target: ['es2022'],
-    plugins: [
-      nodeExternalsPlugin(),
-      vanillaExtractPlugin({}),
-      {
-        name: 'on-end',
-        setup(build) {
-          build.onEnd(({ errors = [] }) => {
-            if (errors.length) {
-              console.error(`build failed for ${packageName}:`, errors);
-            } else {
-              console.log(`build succeeded for ${packageName}:`);
-            }
-          });
-        }
+const buildEndPlugin = () => ({
+  name: 'on-end',
+  setup(build) {
+    build.onEnd(({ errors = [] }) => {
+      if (errors.length) {
+        console.error(`🛠️  - failed for: ${packageName} ❌`, errors);
+      } else if (watchEnabled) {
+        console.log(`🛠️ - succeeded for: ${packageName} ✅,  👀 for changes...`);
+      } else {
+        console.log(`🛠️ - succeeded for: ${packageName} ✅`);
       }
-    ],
-    external: [
-      'react',
-      'react-dom',
-      'next/*',
-      '@jpmorganchase/mosaic-components',
-      '@jpmorganchase/mosaic-components-lab',
-      '@jpmorganchase/mosaic-mdx-components',
-      '@jpmorganchase/mosaic-site-components-next',
-      '@jpmorganchase/mosaic-open-api-component',
-      '@jpmorganchase/mosaic-content-editor-plugin',
-      '@jpmorganchase/mosaic-site-components',
-      '@jpmorganchase/mosaic-site-components-next',
-      '@jpmorganchase/mosaic-layouts',
-      '@jpmorganchase/mosaic-store'
-    ]
-  });
-  await context.rebuild();
-  if (watchEnabled) {
-    await context.watch();
+    });
   }
-  await context.serve();
-  context.dispose();
+});
+
+const esbuildConfig = {
+  entryPoints: glob.sync(['src/**/*.ts?(x)', 'src/*.ts?(x)'], {
+    ignore: ['**/__tests__']
+  }),
+  loader: {
+    '.jpg': 'dataurl',
+    '.png': 'dataurl',
+    '.svg': 'text'
+  },
+  outdir: './dist',
+  bundle: true,
+  sourcemap: true,
+  splitting: true,
+  minify: true,
+  format: 'esm',
+  target: ['es2022'],
+  plugins: [nodeExternalsPlugin(), vanillaExtractPlugin({}), buildEndPlugin()],
+  external: ['react', 'react-dom', 'next/*', '@jpmorganchase/mosaic-*']
+};
+
+try {
+  if (watchEnabled) {
+    const context = await esbuild.context(esbuildConfig);
+    await context.watch();
+  } else {
+    await esbuild.build(esbuildConfig);
+  }
 } catch (e) {
   if (e.errors && e.errors.length > 0) {
     console.group(`!!!!!!! ${packageName} build errors !!!!!!!`);
