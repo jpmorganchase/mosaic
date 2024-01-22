431817e7e13aa3029bd2840fc60c9134f27f1cb4
Davie <reid5587@gmail.com>
2024-01-22 11:15:44 +0000

    refactor: disable salt style injection

--- packages/site-components/src/ThemeProvider.tsx
+++ packages/site-components/src/ThemeProvider.tsx
@@ -1,19 +1,8 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 import { SaltProvider } from '@salt-ds/core';
 import { useColorMode } from '@jpmorganchase/mosaic-store';
-import { config } from '@jpmorganchase/mosaic-theme';
 
 import classnames from 'clsx';
-
-const useHasHydrated = () => {
-  const [hasHydrated, setHasHydrated] = useState(false);
-
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
-
-  return hasHydrated;
-};
 
 interface ThemeProviderProps {
   className?: string;
@@ -21,15 +10,14 @@ interface ThemeProviderProps {
 }
 
 export function ThemeProvider({ className, children }: ThemeProviderProps) {
-  const hasHydrated = useHasHydrated();
   const { colorMode } = useColorMode();
 
-  const ssrClassname = hasHydrated ? undefined : config.ssrClassName;
-
   return (
-    <SaltProvider mode={hasHydrated ? colorMode : 'light'}>
-      <div className={classnames(ssrClassname, className)}>{children}</div>
-      <div data-mosaic-id="portal-root" />
+    <SaltProvider mode={colorMode} enableStyleInjection={false}>
+      <div className={classnames('salt-theme', 'salt-density-medium', className)}>
+        {children}
+        <div data-mosaic-id="portal-root" />
+      </div>
     </SaltProvider>
   );
 }
--- packages/site-preset-styles/src/index.js
+++ packages/site-preset-styles/src/index.js
@@ -1,8 +1,10 @@
 import '@salt-ds/theme/css/global.css';
 import '@salt-ds/theme/css/theme.css';
+import '@salt-ds/icons/css/salt-icon.css';
+import '@salt-ds/core/css/salt-core.css';
+import '@salt-ds/lab/css/salt-lab.css';
 import '@jpmorganchase/mosaic-theme/index.css';
 import '@jpmorganchase/mosaic-theme/baseline.css';
-import '@jpmorganchase/mosaic-theme/salt.css';
 import '@jpmorganchase/mosaic-layouts/index.css';
 import '@jpmorganchase/mosaic-site-components/index.css';
 import '@jpmorganchase/mosaic-components/index.css';
