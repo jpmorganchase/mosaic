c0817d7b2e8cce0fb7f597e6c8a7a8a9e0b9c0c7
Mark Tate <143323+mark-tate@users.noreply.github.com>
2024-02-04 09:16:48 +0000

    feat: add `@mosaic-sitemap-component`
    A visualiser for `sitemap.xml`

---
+++ packages/sitemap-component/README.md
@@ -0,0 +1,12 @@
+# Mosaic Sitemap Component
+
+`@jpmorganchase/mosaic-sitemap-component` a d3 visualiser of sitemap.xml
+
+## Features
+
+1. lazily loads d3 dependencies
+2. renders a tree view of the Mosaic virtual file system derived from sitemap.xml
+
+## Installation
+
+`yarn add @jpmorganchase/mosaic-sitemap-component`
---
+++ packages/sitemap-component/src/SitemapToolbar.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Dropdown, DropdownButton, SelectionChangeHandler } from '@salt-ds/lab';
+import {
+  useToolbarDispatch,
+  useToolbarState,
+  Caption2,
+  Icon
+} from '@jpmorganchase/mosaic-components';
+
+import styles from './styles.css';
+
+export interface SitemapToolbarProps {
+  /** Additional class name for overrides */
+  className?: string;
+  /** The url of the file system */
+  loading: boolean;
+  pageCount: number;
+  namespaces: string[];
+  /** Initial namespace filters */
+  initialNamespaceFilters?: string[];
+}
+
+const defaultButtonLabel = (selectedItems: string[] | undefined) => {
+  if (!selectedItems || selectedItems.length === 0) {
+    return 'All';
+  }
+  if (selectedItems.length === 1) {
+    return `${selectedItems[0]}`;
+  }
+  return `${selectedItems.length} Items Selected`;
+};
+
+export const SitemapToolbar: React.FC<SitemapToolbarProps> = ({
+  initialNamespaceFilters = [],
+  loading,
+  namespaces,
+  pageCount,
+  ...rest
+}: SitemapToolbarProps) => {
+  const dispatch = useToolbarDispatch();
+  const { filters = [] } = useToolbarState();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleSelect: SelectionChangeHandler<string, 'multiple'> = (_e, selectedItems) =>
+    dispatch({ type: 'setFilters', value: selectedItems });
+
+  return (
+    <div className={styles.toolbar}>
+      {!loading ? (
+        <>
+          <Caption2 className={styles.pageCount}>Number of pages: {pageCount}</Caption2>
+          <Dropdown<string, 'multiple'>
+            aria-label={isOpen ? 'close filters menu' : 'open filters menu'}
+            className={styles.filterDropdown}
+            onOpenChange={setIsOpen}
+            onSelectionChange={handleSelect}
+            selected={filters}
+            selectionStrategy="multiple"
+            source={namespaces}
+            triggerComponent={
+              <span className={styles.filterDropdownTriggerRoot}>
+                <Icon name="filter" />
+                <DropdownButton label={defaultButtonLabel(filters)} />
+              </span>
+            }
+            width={200}
+            {...rest}
+          />
+        </>
+      ) : null}
+    </div>
+  );
+};
