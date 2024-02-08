95eb951b2bbbb76a2dac1bef606552baea21134d
Mark Tate <143323+mark-tate@users.noreply.github.com>
2024-02-08 00:15:27 +0000

    sitemap tree supports light/dark theme

---
+++ packages/sitemap-component/src/drawTree/styles.css.ts
@@ -0,0 +1,34 @@
+import { style } from '@vanilla-extract/css';
+import { link } from '@jpmorganchase/mosaic-theme';
+
+export default {
+  link: style([
+    link({ variant: 'regular' }),
+    { fill: 'var(--salt-palette-interact-secondary-foreground)', strokeWidth: 0 }
+  ]),
+  tree: style({
+    maxWidth: '100%',
+    height: 'auto',
+    overflow: 'visible',
+    stroke: 'var(--salt-palette-track-border)',
+    fontFamily: 'sans-serif',
+    fontSize: 10
+  }),
+  line: style({
+    strokeOpacity: 1.5,
+    strokeWidth: 0.4,
+    fill: 'none'
+  }),
+  nodeParent: style({
+    strokeOpacity: 1.5,
+    strokeWidth: 0.4,
+    stroke: 'var(--salt-palette-positive-foreground)',
+    fill: 'var(--salt-palette-positive-foreground)'
+  }),
+  nodeChild: style({
+    strokeOpacity: 1.5,
+    strokeWidth: 0.4,
+    stroke: 'var(--salt-palette-negative-foreground)',
+    fill: 'var(--salt-palette-negative-foreground)'
+  })
+};
