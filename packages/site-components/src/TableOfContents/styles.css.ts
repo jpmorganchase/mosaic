d8a45d330214d6984550a22e4411d26fc4d53ca3
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-02-22 12:09:21 +0000

    Fixed table of contents indentation

--- packages/site-components/src/TableOfContents/styles.css.ts
+++ packages/site-components/src/TableOfContents/styles.css.ts
@@ -1,5 +1,5 @@
 import { style } from '@vanilla-extract/css';
-import { link, navigableBorder, responsiveSprinkles } from '@jpmorganchase/mosaic-theme';
+import { responsiveSprinkles } from '@jpmorganchase/mosaic-theme';
 
 export default {
   list: style([
@@ -11,42 +11,5 @@ export default {
       marginBottom: ['x6', 'x6', 'x6', 'x6'],
       padding: ['none', 'none', 'none', 'none']
     })
-  ]),
-  item: style([
-    {
-      cursor: 'pointer',
-      position: 'relative',
-      display: 'block'
-    },
-    navigableBorder({
-      variant: 'unselected',
-      borderLeftWidth: 'medium'
-    }),
-    navigableBorder({
-      variant: 'hover',
-      borderLeftWidth: 'medium'
-    }),
-    navigableBorder({
-      variant: 'selected',
-      borderLeftWidth: 'medium'
-    }),
-    link({ variant: 'selectable' }),
-    responsiveSprinkles({
-      paddingTop: ['x1', 'x1', 'x1', 'x1'],
-      paddingRight: ['x4', 'x4', 'x4', 'x4'],
-      paddingBottom: ['x1', 'x1', 'x1', 'x1']
-    })
-  ]),
-  level1: responsiveSprinkles({
-    paddingLeft: ['x2', 'x2', 'x2', 'x2']
-  }),
-  level2: responsiveSprinkles({
-    paddingLeft: ['x4', 'x4', 'x4', 'x4']
-  }),
-  level3: responsiveSprinkles({
-    paddingLeft: ['x6', 'x6', 'x6', 'x6']
-  }),
-  level4: responsiveSprinkles({
-    paddingLeft: ['x8', 'x8', 'x8', 'x8']
-  })
+  ])
 };
